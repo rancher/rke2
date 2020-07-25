0a17742e0c99369b7db34b6fde517a7132189016
Brian Downs <brian.downs@gmail.com>
2020-07-24 21:26:28 -0700

    remove unnecessary code
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- pkg/staticpod/staticpod.go
+++ pkg/staticpod/staticpod.go
@@ -13,7 +13,6 @@ import (
 	"strings"
 
 	"github.com/rancher/wrangler/pkg/yaml"
-	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -63,7 +62,6 @@ func Run(dir string, args Args) error {
 }
 
 func writeFile(dir, name string, content []byte) error {
-	logrus.Warnf("writeFile() - creating director: %s/%s", dir, name)
 	if err := os.MkdirAll(dir, 0700); err != nil {
 		return err
 	}
