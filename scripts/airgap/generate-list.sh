366ef3d51a561e1ba66db9cdf0745d878ce38c31
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2020-08-04 21:23:30 +0200

    fix typo
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- scripts/airgap/generate-list.sh
+++ scripts/airgap/generate-list.sh
@@ -3,7 +3,7 @@ set -e -x
 
 cd $(dirname $0)
 
-export CONTAINER_RUNTIME_ENDPOINT=/run/k3s/containerd/containerd.scok
+export CONTAINER_RUNTIME_ENDPOINT=/run/k3s/containerd/containerd.sock
 crictl images -o json \
     | jq -r '.images[].repoTags[0] | select(. != null)' \
     | tee image-list.txt
