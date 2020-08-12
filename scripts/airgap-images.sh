0d11bdc30bc022f3e1dd7fa2788980a4199ca00b
Brian Downs <brian.downs@gmail.com>
2020-08-12 14:06:05 -0700

    zip tarballs
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- scripts/airgap-images.sh
+++ scripts/airgap-images.sh
@@ -9,5 +9,4 @@ mkdir -p ./build/images/
 airgap_image_file='scripts/airgap/image-list.txt'
 images=$(cat "${airgap_image_file}")
 xargs -n1 docker pull <<< "${images}"
-docker save ${images} -o ./build/images/airgap.tar
-
+docker save ${images} | gzip > ./build/images/airgap.tar.gz
