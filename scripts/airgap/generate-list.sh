a1441e2279a0e77cc632c543c24351e3d0244457
Jacob Blain Christen <jacob@rancher.com>
2020-08-20 05:30:10 -0700

    airgap: fix images listing (#211)
    The rancher/kubernetes and rancher/rke2-runtimes images are missing the
    pull repository path (aka I forgot to prefix with docker.io/). This
    change fixes that.

--- scripts/airgap/generate-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e -x
-
-cd $(dirname $0)
-
-export CONTAINER_RUNTIME_ENDPOINT=/run/k3s/containerd/containerd.sock
-crictl images -o json \
-    | jq -r '.images[].repoTags[0] | select(. != null)' \
-    | tee image-list.txt
