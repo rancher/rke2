98dcfd2abfcb4e90a89526aec9ad644693743f92
JenTing Hsiao <jenting.hsiao@suse.com>
2020-12-22 14:52:12 +0800

    Align shell convention
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- scripts/codespell.sh
+++ scripts/codespell.sh
@@ -5,11 +5,8 @@ set -e
 # Note: ignore ".te#" in https://github.com/rancher/rke2/blob/eb79cc8/docs/security/selinux.md#L13,L17-L19
 codespell --skip=.git,./vendor --check-filenames --ignore-regex=.te#
 
-res=`echo $?`
-if [ "$res" = "0" ]; then
-    echo "Codespell found no problems"
-else
-    echo "Codespell found one or more problems"
+code=$?
+if [ $code -ne 0 ]; then
+  echo "Error: codespell found one or moe problems!"
+  exit $code
 fi
-
-exit $res
