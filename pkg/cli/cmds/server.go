ee8ee998f875b553fa50607526dc648f08b6641f
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2021-02-12 20:10:51 +0200

    add disable flags to rke2

--- pkg/cli/cmds/server.go
+++ pkg/cli/cmds/server.go
@@ -59,10 +59,13 @@ var (
 			Usage: "(components) Do not deploy packaged components and delete any deployed components (valid items: " + DisableItems + ")",
 		},
 		"disable-selinux":             drop,
-		"disable-scheduler":           drop,
-		"disable-cloud-controller":    drop,
+		"disable-scheduler":           copy,
+		"disable-cloud-controller":    copy,
 		"disable-network-policy":      drop,
 		"disable-kube-proxy":          drop,
+		"disable-api-server":          copy,
+		"disable-controller-manager":  copy,
+		"disable-etcd":                copy,
 		"etcd-disable-snapshots":      copy,
 		"etcd-snapshot-schedule-cron": copy,
 		"etcd-snapshot-retention":     copy,
@@ -94,6 +97,8 @@ var (
 		"protect-kernel-defaults":     copy,
 		"snapshotter":                 copy,
 		"selinux":                     copy,
+		"service-node-port-range":     copy,
+		"etcd-expose-metrics":         copy,
 	})
 )
 
