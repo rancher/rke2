346558ee61beb88d914e14e9a5a670ed23d7316b
MonzElmasry <menna.elmasry@rancher.com>
2021-03-19 03:40:32 +0200

    update rke2 version to v1.19.9+rke2r1

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -15,7 +15,7 @@ const (
 )
 
 var (
-	KubernetesVersion = "v1.19.8"                    // make sure this matches what is in the scripts/version.sh script
+	KubernetesVersion = "v1.19.9"                    // make sure this matches what is in the scripts/version.sh script
 	PauseVersion      = "3.2"                        // make sure this matches what is in the scripts/build-images script
 	EtcdVersion       = "v3.4.13-k3s1-build20210223" // make sure this matches what is in the scripts/build-images script
 	RuntimeImageName  = "rke2-runtime"
