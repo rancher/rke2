a7dde807a7be79ed1dd49f78d187c860809ef0a7
Manuel Buil <mbuil@suse.com>
2021-04-30 13:01:55 +0200

    Add calico as cni
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- charts/build-chart.sh
+++ charts/build-chart.sh
@@ -4,7 +4,12 @@ set -eux -o pipefail
 
 : "${CHART_FILE?required}"
 : "${CHART_NAME:="$(basename "${CHART_FILE%%.yaml}")"}"
-: "${CHART_URL:="${CHART_REPO:="https://rke2-charts.rancher.io"}/assets/${CHART_NAME}/${CHART_NAME}-${CHART_VERSION:="v0.0.0"}.tgz"}"
+# Assuming charts that only contain crds are called $CHART_MAIN_NAME-crd
+if [[ ${CHART_NAME} = *crd* ]]; then
+    : "${CHART_URL:="${CHART_REPO:="https://rke2-charts.rancher.io"}/assets/${CHART_NAME%%-crd}/${CHART_NAME}-${CHART_VERSION:="v0.0.0"}.tgz"}"
+else
+    : "${CHART_URL:="${CHART_REPO:="https://rke2-charts.rancher.io"}/assets/${CHART_NAME}/${CHART_NAME}-${CHART_VERSION:="v0.0.0"}.tgz"}"
+fi
 curl -fsSL "${CHART_URL}" -o "${CHART_TMP:=$(mktemp)}"
 cat <<-EOF > "${CHART_FILE}"
 apiVersion: helm.cattle.io/v1
