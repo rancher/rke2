3bfb87287b1f398ba4ae7e3c8835face1a6b7287
Brian Downs <brian.downs@gmail.com>
2021-05-18 13:57:20 -0700

    add retention default and wire in s3 prune
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- pkg/cli/cmds/etcd_snapshot.go
+++ pkg/cli/cmds/etcd_snapshot.go
@@ -2,38 +2,51 @@ package cmds
 
 import (
 	"github.com/rancher/k3s/pkg/cli/cmds"
+	"github.com/rancher/k3s/pkg/cli/etcdsnapshot"
 	"github.com/rancher/rke2/pkg/rke2"
 	"github.com/urfave/cli"
 )
 
+var k3sFlags = map[string]*K3SFlagOption{
+	"debug":           copy,
+	"log":             copy,
+	"alsologtostderr": copy,
+	"node-name":       copy,
+	"data-dir": {
+		Usage:   "(data) Folder to hold state",
+		Default: rke2Path,
+	},
+	"name":               copy,
+	"dir":                copy,
+	"s3":                 copy,
+	"s3-endpoint":        copy,
+	"s3-endpoint-ca":     copy,
+	"s3-skip-ssl-verify": copy,
+	"s3-access-key":      copy,
+	"s3-secret-key":      copy,
+	"s3-bucket":          copy,
+	"s3-region":          copy,
+	"s3-folder":          copy,
+}
+
+var subcommands = []cli.Command{
+	{
+		Name:            "ls",
+		Aliases:         []string{"list", "l"},
+		Usage:           "List snapshots",
+		SkipFlagParsing: false,
+		SkipArgReorder:  true,
+		Action:          etcdsnapshot.List,
+		Flags:           cmds.EtcdSnapshotFlags,
+	},
+}
+
 var (
-	k3sEtcdSnapshotBase = mustCmdFromK3S(cmds.NewEtcdSnapshotCommand(EtcdSnapshotRun, []cli.Command{}), map[string]*K3SFlagOption{
-		"debug":           copy,
-		"log":             copy,
-		"alsologtostderr": copy,
-		"node-name":       copy,
-		"data-dir": {
-			Usage:   "(data) Folder to hold state",
-			Default: rke2Path,
-		},
-		"name":               copy,
-		"dir":                copy,
-		"s3":                 copy,
-		"s3-endpoint":        copy,
-		"s3-endpoint-ca":     copy,
-		"s3-skip-ssl-verify": copy,
-		"s3-access-key":      copy,
-		"s3-secret-key":      copy,
-		"s3-bucket":          copy,
-		"s3-region":          copy,
-		"s3-folder":          copy,
-	})
+	k3sEtcdSnapshotBase = mustCmdFromK3S(cmds.NewEtcdSnapshotCommand(EtcdSnapshotRun, subcommands), k3sFlags)
 )
 
 func NewEtcdSnapshotCommand() cli.Command {
-	cmd := k3sEtcdSnapshotBase
-	cmd.Flags = append(cmd.Flags, commonFlag...)
-	return cmd
+	return k3sEtcdSnapshotBase
 }
 
 func EtcdSnapshotRun(clx *cli.Context) error {
