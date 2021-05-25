f05e53d8fd98bed025b0c0af8b16ba3e9125c98c
Jamie Phillips <jamie.phillips@suse.com>
2021-05-25 16:13:23 -0400

    Initial build working on Windows.

---
+++ pkg/rke2/rke2_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package rke2
+
+import (
+	"github.com/urfave/cli"
+)
+
+func Server(clx *cli.Context, cfg Config) error {
+	return nil
+}
+
+func Agent(clx *cli.Context, cfg Config) error {
+	return nil
+}
+
+func EtcdSnapshot(clx *cli.Context, cfg Config) error {
+	return nil
+}
