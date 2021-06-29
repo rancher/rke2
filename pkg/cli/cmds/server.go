6992c01ecbc300025efb8b2143e67b7c07853f6b
Chris Kim <oats87g@gmail.com>
2021-06-29 13:54:54 -0700

    Bump K3s version in go.mod for release-1.20
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/cli/cmds/server.go
+++ pkg/cli/cmds/server.go
@@ -127,6 +127,7 @@ var (
 		"etcd-s3-bucket":                    copy,
 		"etcd-s3-region":                    copy,
 		"etcd-s3-folder":                    copy,
+		"disable-helm-controller":           drop,
 	})
 )
 
