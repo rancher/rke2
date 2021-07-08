0ff5d30b51894305c07068c59ada2c329d3b427a
Derek Nola <derek.nola@suse.com>
2021-07-08 08:40:27 -0700

    Fixed nits and other PR comments
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/rke2/serviceaccount.go
+++ pkg/rke2/serviceaccount.go
@@ -13,29 +13,29 @@ import (
 
 func restrictServiceAccount(ctx context.Context, namespace string, cs *kubernetes.Clientset) error {
 	var sa *v1.ServiceAccount
-	var err error
-	for {
-		sa, err = cs.CoreV1().ServiceAccounts(namespace).Get(ctx, "default", metav1.GetOptions{})
-		if err != nil {
-			if apierrors.IsNotFound(err) {
-				logrus.Infof("namespace: %s: serviceaccount: default does not yet exist", namespace)
-				time.Sleep(5 * time.Second)
-				continue
+	{
+		var err error
+		for {
+			sa, err = cs.CoreV1().ServiceAccounts(namespace).Get(ctx, "default", metav1.GetOptions{})
+			if err != nil {
+				if apierrors.IsNotFound(err) {
+					logrus.Infof("namespace: %s: serviceaccount: default does not yet exist", namespace)
+					time.Sleep(5 * time.Second)
+					continue
+				}
+				return err
 			}
-			return err
+			break
 		}
-		break
 	}
-	if sa.AutomountServiceAccountToken == nil {
-		sa.AutomountServiceAccountToken = new(bool)
-	}
-	*sa.AutomountServiceAccountToken = false
-	_, err = cs.CoreV1().ServiceAccounts(namespace).Update(ctx, sa, metav1.UpdateOptions{})
+	var automount = false
+	sa.AutomountServiceAccountToken = &automount
+	_, err := cs.CoreV1().ServiceAccounts(namespace).Update(ctx, sa, metav1.UpdateOptions{})
 	return err
 }
 
 // restrictServiceAccounts disables automount across the 3 primary namespaces.
-func restrictServiceAccounts(cisMode bool) func(context.Context, <-chan struct{}, string) error {
+func restrictServiceAccounts(cisMode bool, namespaces []string) func(context.Context, <-chan struct{}, string) error {
 	return func(ctx context.Context, apiServerReady <-chan struct{}, kubeConfigAdmin string) error {
 		if cisMode {
 			logrus.Info("Restricting automount...")
@@ -45,18 +45,13 @@ func restrictServiceAccounts(cisMode bool) func(context.Context, <-chan struct{}
 				if err != nil {
 					logrus.Fatalf("serviceAccount: new k8s client: %s", err.Error())
 				}
-				var namespaces = []string{
-					metav1.NamespaceDefault,
-					metav1.NamespaceSystem,
-					metav1.NamespacePublic,
-					"kube-node-lease",
-				}
-				for _, namespace := range namespaces {
+				nps := append(namespaces, "kube-node-lease")
+				for _, namespace := range nps {
 					if err := restrictServiceAccount(ctx, namespace, cs); err != nil {
 						logrus.Fatalf("serviceAccount: namespace %s %s", namespace, err.Error())
 					}
 				}
-				logrus.Info("Restricting automount for serviceAccounts complete")
+				logrus.Info("Restricting automount for default serviceAccounts complete")
 			}()
 		}
 		return nil
