9880291e31f7c9815893d26ada76f2972102d3dc
Luther Monson <luther.monson@gmail.com>
2021-07-19 18:38:09 -0700

    switch to using start up hooks args

--- pkg/rke2/kp.go
+++ pkg/rke2/kp.go
@@ -2,7 +2,6 @@ package rke2
 
 import (
 	"context"
-	"sync"
 
 	"github.com/k3s-io/helm-controller/pkg/generated/controllers/helm.cattle.io"
 	"github.com/rancher/k3s/pkg/cli/cmds"
@@ -17,14 +16,14 @@ const kubeProxyChart = "rke2-kube-proxy"
 
 // setKubeProxyDisabled determines if a cluster already has kube proxy deployed as a chart, if so
 // disables running kubeproxy as a static pod.
-func setKubeProxyDisabled(clx *cli.Context, cfg *cmds.Server) func(context.Context, *sync.WaitGroup, <-chan struct{}, string) error {
-	return func(ctx context.Context, wg *sync.WaitGroup, apiServerReady <-chan struct{}, kubeConfigAdmin string) error {
+func setKubeProxyDisabled(clx *cli.Context, cfg *cmds.Server) cmds.StartupHook {
+	return func(ctx context.Context, args cmds.StartupHookArgs) error {
 		go func() {
-			defer wg.Done()
-			<-apiServerReady
+			defer args.Wg.Done()
+			<-args.APIServerReady
 			logrus.Info("Checking for kube proxy as a chart")
 
-			restConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigAdmin)
+			restConfig, err := clientcmd.BuildConfigFromFlags("", args.KubeConfigAdmin)
 			if err != nil {
 				logrus.Fatalf("kp: new k8s client: %s", err.Error())
 			}
@@ -36,7 +35,8 @@ func setKubeProxyDisabled(clx *cli.Context, cfg *cmds.Server) func(context.Conte
 			if _, err := hc.Helm().V1().HelmChartConfig().Get(metav1.NamespaceSystem, kubeProxyChart, metav1.GetOptions{}); err != nil {
 				if apierrors.IsNotFound(err) {
 					logrus.Infof("%[1]s HelmChartConfig not found, disabling %[1]s", kubeProxyChart)
-					clx.Set("disable", kubeProxyChart)
+					args.Skips[kubeProxyChart] = true
+					args.Disables[kubeProxyChart] = true
 					return
 				}
 				logrus.WithError(err).Fatalf("kp: failed to check for %s HelmChartConfig", kubeProxyChart)
