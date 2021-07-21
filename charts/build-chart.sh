7cec5c3643fca1bafac344ba00272549e11217f1
Brad Davidson <brad.davidson@rancher.com>
2021-07-21 11:40:28 -0700

    Bump kubernetes versions to GA build
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- charts/build-chart.sh
+++ charts/build-chart.sh
@@ -4,7 +4,8 @@ set -eux -o pipefail
 
 : "${CHART_FILE?required}"
 : "${CHART_NAME:="$(basename "${CHART_FILE%%.yaml}")"}"
-: "${CHART_URL:="${CHARTS_REPO:="https://rke2-charts.rancher.io"}/assets/${CHART_NAME}/${CHART_NAME}-${CHART_VERSION:="v0.0.0"}.tgz"}"
+: "${CHART_PACKAGE:="${CHART_NAME}"}"
+: "${CHART_URL:="${CHART_REPO:="https://rke2-charts.rancher.io"}/assets/${CHART_PACKAGE}/${CHART_NAME}-${CHART_VERSION:="v0.0.0"}.tgz"}"
 curl -fsSL "${CHART_URL}" -o "${CHART_TMP:=$(mktemp)}"
 cat <<-EOF > "${CHART_FILE}"
 apiVersion: helm.cattle.io/v1
