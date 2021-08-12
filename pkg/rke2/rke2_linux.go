d28f509dcb57d92eb8e93234d8df4263c5d4e428
Chris Kim <oats87g@gmail.com>
2021-08-12 14:23:50 -0700

    Add missing kubeproxy extra env and mounts
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- pkg/rke2/rke2_linux.go
+++ pkg/rke2/rke2_linux.go
@@ -150,6 +150,7 @@ func initExecutor(clx *cli.Context, cfg Config, dataDir string, disableETCD bool
 		KubeAPIServer:          cfg.ExtraEnv.KubeAPIServer.Value(),
 		KubeScheduler:          cfg.ExtraEnv.KubeScheduler.Value(),
 		KubeControllerManager:  cfg.ExtraEnv.KubeControllerManager.Value(),
+		KubeProxy:              cfg.ExtraEnv.KubeProxy.Value(),
 		Etcd:                   cfg.ExtraEnv.Etcd.Value(),
 		CloudControllerManager: cfg.ExtraEnv.CloudControllerManager.Value(),
 	}
@@ -158,6 +159,7 @@ func initExecutor(clx *cli.Context, cfg Config, dataDir string, disableETCD bool
 		KubeAPIServer:          cfg.ExtraMounts.KubeAPIServer.Value(),
 		KubeScheduler:          cfg.ExtraMounts.KubeScheduler.Value(),
 		KubeControllerManager:  cfg.ExtraMounts.KubeControllerManager.Value(),
+		KubeProxy:              cfg.ExtraMounts.KubeProxy.Value(),
 		Etcd:                   cfg.ExtraMounts.Etcd.Value(),
 		CloudControllerManager: cfg.ExtraMounts.CloudControllerManager.Value(),
 	}
