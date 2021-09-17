6cadce506e7930523ee0556c2e16c1701a5894a7
Brian Downs <brian.downs@gmail.com>
2021-09-17 10:34:48 -0700

    address pr comments
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- pkg/podexecutor/staticpod.go
+++ pkg/podexecutor/staticpod.go
@@ -276,7 +276,7 @@ func (s *StaticPodConfig) APIServer(ctx context.Context, etcdReady <-chan struct
 	return auth, http.NotFoundHandler(), err
 }
 
-const permitPortSharingFlag = "--permit-port-sharing=true"
+var permitPortSharingFlag = []string{"--permit-port-sharing=true"}
 
 // Scheduler starts the kube-scheduler static pod, once the apiserver is available.
 func (s *StaticPodConfig) Scheduler(apiReady <-chan struct{}, args []string) error {
@@ -294,7 +294,7 @@ func (s *StaticPodConfig) Scheduler(apiReady <-chan struct{}, args []string) err
 	if s.ControlPlaneResources.KubeSchedulerCPURequest == "" {
 		s.ControlPlaneResources.KubeSchedulerCPURequest = defaultKubeSchedulerCPURequest
 	}
-	args = append(args, permitPortSharingFlag)
+	args = append(permitPortSharingFlag, args...)
 	return after(apiReady, func() error {
 		return staticpod.Run(s.ManifestsDir, staticpod.Args{
 			Command:       "kube-scheduler",
@@ -351,7 +351,7 @@ func (s *StaticPodConfig) ControllerManager(apiReady <-chan struct{}, args []str
 		}
 		args = append(extraArgs, args...)
 	}
-	args = append(args, permitPortSharingFlag)
+	args = append(permitPortSharingFlag, args...)
 
 	files := []string{}
 	if !s.DisableETCD {
