bb37f5f75c61ad0781c60487381bb2df4332b5a3
dereknola <derek.nola@suse.com>
2021-10-04 12:17:57 -0700

    go mod vendor pt3
    Signed-off-by: dereknola <derek.nola@suse.com>

---
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "21"
+	gitVersion   = "v1.21.4-k3s1"
+	gitCommit    = "24d3954c6cd8b757150dd3b0ebf02ad005f07523"
+	gitTreeState = "clean"
+	buildDate = "2021-08-12T22:35:54Z"
+)
