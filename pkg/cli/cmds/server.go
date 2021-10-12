91245c0e9262f3dcab9b6e1e30bc4a6032633df9
Brad Davidson <brad.davidson@rancher.com>
2021-10-12 16:36:24 -0700

    Update K3s and update executors to delay etcd join
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cli/cmds/server.go
+++ pkg/cli/cmds/server.go
@@ -126,6 +126,8 @@ var (
 		"etcd-s3-bucket":                    copy,
 		"etcd-s3-region":                    copy,
 		"etcd-s3-folder":                    copy,
+		"etcd-s3-insecure":                  copy,
+		"etcd-s3-timeout":                   copy,
 		"disable-helm-controller":           drop,
 	})
 )
