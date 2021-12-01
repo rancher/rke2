b2e9a6ce4a613809206341bccee4ecff01b73f7d
Derek Nola <derek.nola@suse.com>
2021-12-01 11:35:29 -0800

    Add rke2 valid flags
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/cmds/agent.go
+++ pkg/cli/cmds/agent.go
@@ -2,6 +2,7 @@ package cmds
 
 import (
 	"github.com/rancher/k3s/pkg/cli/cmds"
+	"github.com/rancher/k3s/pkg/configfilearg"
 	"github.com/rancher/rke2/pkg/rke2"
 	"github.com/urfave/cli"
 )
@@ -62,6 +63,7 @@ func NewAgentCommand() cli.Command {
 	cmd := k3sAgentBase
 	cmd.Flags = append(cmd.Flags, commonFlag...)
 	cmd.Flags = append(cmd.Flags, deprecatedFlags...)
+	configfilearg.DefaultParser.ValidFlags[cmd.Name] = cmd.Flags
 	return cmd
 }
 
