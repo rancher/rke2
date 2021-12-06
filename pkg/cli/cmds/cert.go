394a94972a48695b6ea2908fbbc549fcb09f9890
Hussein Galal <galal-hussein@users.noreply.github.com>
2021-12-07 01:48:15 +0200

    Adding certificate rotation command to rke2
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

---
+++ pkg/cli/cmds/cert.go
@@ -0,0 +1,55 @@
+package cmds
+
+import (
+	"io/ioutil"
+
+	"github.com/rancher/k3s/pkg/cli/cert"
+	"github.com/rancher/k3s/pkg/cli/cmds"
+	"github.com/rancher/k3s/pkg/configfilearg"
+	"github.com/rancher/rke2/pkg/rke2"
+	"github.com/urfave/cli"
+)
+
+var k3sCertFlags = map[string]*K3SFlagOption{
+	"config":          copy,
+	"debug":           copy,
+	"log":             copy,
+	"alsologtostderr": copy,
+	"service":         copy,
+	"data-dir": {
+		Usage:   "(data) Folder to hold state",
+		Default: rke2Path,
+	},
+}
+
+var certSubcommands = []cli.Command{
+	{
+		Name:            "rotate",
+		Usage:           "Certificate Rotatation",
+		SkipFlagParsing: false,
+		SkipArgReorder:  true,
+		Action:          CertificateRotationRun,
+		Flags:           cmds.CertCommandFlags,
+	},
+}
+
+var (
+	k3sCertBase = mustCmdFromK3S(cmds.NewCertCommand(certSubcommands), k3sCertFlags)
+)
+
+func NewCertRotateCommand() cli.Command {
+	cmd := k3sCertBase
+	configfilearg.DefaultParser.ValidFlags[cmd.Name] = cmd.Flags
+	return cmd
+}
+
+func CertificateRotationRun(clx *cli.Context) error {
+	dataDir := clx.String("data-dir")
+	if dataDir == "" {
+		dataDir = rke2Path
+	}
+	if err := ioutil.WriteFile(rke2.ForceRestartFile(dataDir), []byte{}, 0600); err != nil {
+		return err
+	}
+	return cert.Run(clx)
+}
