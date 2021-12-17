5dcbb73ccb223d6a8d426159ba5c318e71a9de02
Brad Davidson <brad.davidson@rancher.com>
2021-12-16 16:00:47 -0800

    Ignore go.mod/go.sum in codespell
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/codespell.sh
+++ scripts/codespell.sh
@@ -3,7 +3,7 @@ set -e
 
 # Ignore vendor folder and check file names as well
 # Note: ignore ".te#" in https://github.com/rancher/rke2/blob/eb79cc8/docs/security/selinux.md#L13,L17-L19
-codespell --skip=.git,./vendor --check-filenames --ignore-regex=.te# --ignore-words=.codespellignore
+codespell --skip=.git,./vendor,./MAINTAINERS,go.mod,go.sum --check-filenames --ignore-regex=.te# --ignore-words=.codespellignore
 
 code=$?
 if [ $code -ne 0 ]; then
