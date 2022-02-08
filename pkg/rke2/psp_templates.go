ab44cb426a5cc5ce4fa58c6ef1247f062cce890b
Brian Downs <brian.downs@gmail.com>
2022-02-07 19:39:13 -0700

    remove erroneous apparmor annotation
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- pkg/rke2/psp_templates.go
+++ pkg/rke2/psp_templates.go
@@ -88,7 +88,6 @@ metadata:
   annotations:
     psp.rke2.io/global-unrestricted: resolved
     seccomp.security.alpha.kubernetes.io/allowedProfileNames: '*'
-    apparmor.security.beta.kubernetes.io/allowedProfileNames: '*'
 spec:
   privileged: true
   allowPrivilegeEscalation: true
