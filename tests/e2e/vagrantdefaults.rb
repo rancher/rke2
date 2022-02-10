5c997f007a0313fe236ccb59ce27395ae78eab1d
Derek Nola <derek.nola@suse.com>
2022-02-10 14:08:39 -0800

    add vagrant common code
    Signed-off-by: Derek Nola <derek.nola@suse.com>

---
+++ tests/e2e/vagrantdefaults.rb
@@ -0,0 +1,11 @@
+def defaultOSConfigure(vm)
+
+    if vm.box.include?("ubuntu2004")
+      vm.provision "shell", inline: "systemd-resolve --set-dns=8.8.8.8 --interface=eth0", run: 'once'
+      vm.provision "shell", inline: "apt install -y jq", run: 'once'
+    end
+    if vm.box.include?("Leap")
+      vm.provision "shell", inline: "zypper install -y jq", run: 'once'
+    end
+  
+  end
