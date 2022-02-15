99c39f7864436a1710041b5bdc41844f419268a5
Derek Nola <derek.nola@suse.com>
2022-02-15 14:41:53 -0800

    Fix to ipv6 server
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/ipv6.sh
+++ tests/e2e/scripts/ipv6.sh
@@ -5,6 +5,30 @@ ip6_addr=$2
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.disable_ipv6=0
 ip -6 addr add "$ip6_addr"/64 dev eth1
-sleep 5
+# ip -6 route add default via "$ip6_prefix"::1
+# Wait for dhcp6 link to come up and remove it
+iterations=0
+numlinks="$(ip -6 addr show to "$ip6_prefix"::/64 | grep -oP 'inet6 \K(\S+)' | wc -l)"
+while [ "$numlinks" -ne 2 ]; do
+    ((iterations++))
+    if [ "$iterations" -ge 60 ]; then
+        echo "dhcp6 address never came up"
+        exit 1
+    fi
+    sleep 1
+    numlinks="$(ip -6 addr show to "$ip6_prefix"::/64 | grep -oP 'inet6 \K(\S+)' | wc -l)"
+done
 dhcplink=$(ip -6 addr show to "$ip6_prefix"::/64 | grep -oP 'inet6 \K(\S+)' | grep "$ip6_prefix:[0-9].*")
 ip -6 addr del "$dhcplink" dev eth1
+
+# Override default canal and specify the interface since we don't have a default IPv6 route
+mkdir -p /var/lib/rancher/rke2/server/manifests
+echo "apiVersion: helm.cattle.io/v1
+kind: HelmChartConfig
+metadata:
+  name: rke2-canal
+  namespace: kube-system
+spec:
+  valuesContent: |-
+    flannel:
+      iface: \"eth1\"" >> /var/lib/rancher/rke2/server/manifests/canal.yaml
