151d4c02b2efdd082a58d9516b4bf4e1de5b7621
Derek Nola <derek.nola@suse.com>
2022-03-04 12:48:50 -0800

    Backout iterations
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/latest_commit.ps1
+++ tests/e2e/scripts/latest_commit.ps1
@@ -1,12 +1,19 @@
 $CommitFile = "./commits.txt"
-# (Invoke-RestMethod https://api.github.com/repos/rancher/rke2/commits?per_page=5).sha | `
-# Out-File -FilePath $CommitFile
+(Invoke-RestMethod https://api.github.com/repos/rancher/rke2/commits?per_page=5).sha | `
+Out-File -FilePath $CommitFile
 
 $StorageUrl = "https://storage.googleapis.com/rke2-ci-builds/rke2-images.windows-amd64-"
-$TopCommit = (head -n 1 $CommitFile)
+$TopCommit = (Get-Content -TotalCount 1 $CommitFile)
 $StatusCode = Invoke-WebRequest $StorageUrl$TopCommit".tar.zst.sha256sum" -DisableKeepAlive -UseBasicParsing -Method head | % {$_.StatusCode}
-while ($StatusCode -ne 200 ) {
+$Iterations = 0
+while (($StatusCode -ne 200) -AND ($Iterations -lt 6)) {
+    $Iterations++
     (Get-Content $CommitFile | Select-Object -Skip 1) | Set-Content $CommitFile
-    $TopCommit = (head -n 1 $CommitFile)
+    $TopCommit = (Get-Content -TotalCount 1 $CommitFile)
     $StatusCode = Invoke-WebRequest $StorageUrl$TopCommit".tar.zst.sha256sum" -DisableKeepAlive -UseBasicParsing -Method head | % {$_.StatusCode}
+}
+
+if ($Iterations -ge 6){
+    Write-Host echo "No valid commits found" 
+    Exit 1
 }
