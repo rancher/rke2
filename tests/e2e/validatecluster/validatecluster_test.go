0e9efd521c5de31967cc85c1e348621506f2b1f8
Derek Nola <derek.nola@suse.com>
2022-03-28 16:17:39 -0700

    Update all E2E test to use external ENV vars

--- tests/e2e/validatecluster/validatecluster_test.go
+++ tests/e2e/validatecluster/validatecluster_test.go
@@ -17,8 +17,8 @@ var nodeOS = flag.String("nodeOS", "generic/ubuntu2004", "VM operating system")
 var serverCount = flag.Int("serverCount", 3, "number of server nodes")
 var agentCount = flag.Int("agentCount", 1, "number of agent nodes")
 
-// Valid format: RELEASE_VERSION=v1.22.6+rke2r1 or nil for latest commit from master
-var installType = flag.String("installType", "", "a string")
+// Environment Variables Info:
+// E2E_RELEASE_VERSION=v1.23.1+rke2r1 or nil for latest commit from master
 
 func Test_E2EClusterValidation(t *testing.T) {
 	flag.Parse()
@@ -36,7 +36,7 @@ var _ = Describe("Verify Basic Cluster Creation", func() {
 
 	It("Starts up with no issues", func() {
 		var err error
-		serverNodeNames, agentNodeNames, err = e2e.CreateCluster(*nodeOS, *serverCount, *agentCount, *installType)
+		serverNodeNames, agentNodeNames, err = e2e.CreateCluster(*nodeOS, *serverCount, *agentCount)
 		Expect(err).NotTo(HaveOccurred(), e2e.GetVagrantLog())
 		fmt.Println("CLUSTER CONFIG")
 		fmt.Println("OS:", *nodeOS)
