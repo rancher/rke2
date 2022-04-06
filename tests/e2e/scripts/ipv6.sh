abd8fd33635089de75ad98a54fbc3eff02905d62
Derek Nola <derek.nola@suse.com>
2022-04-06 14:31:52 -0700

    E2E Dualstack CNI Support (#2730)
    * Added switch case for CNIs
    * Curl  fixes, apply manifests correctly
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/ipv6.sh
+++ tests/e2e/scripts/ipv6.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
-ip6_addr=$1
-
-netplan set ethernets.eth1.accept-ra=false
-netplan apply
+ip4_addr=$1
+ip6_addr=$2
+cni=$3
+os=$4
 
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.eth1.accept_dad=0
-ip -6 addr add "$ip6_addr"/64 dev eth1
-ip addr show dev eth1
-# Override default canal and specify the interface since we don't have a default IPv6 route
-mkdir -p /var/lib/rancher/rke2/server/manifests
-# echo "apiVersion: helm.cattle.io/v1
-# kind: HelmChartConfig
-# metadata:
-#   name: rke2-canal
-#   namespace: kube-system
-# spec:
-#   valuesContent: |-
-#     flannel:
-#       iface: \"eth1\"" >> /var/lib/rancher/rke2/server/manifests/e2e-canal.yaml
 
-echo "apiVersion: helm.cattle.io/v1
+if [ -z "${os##*ubuntu*}" ]; then
+  netplan set ethernets.eth1.accept-ra=false
+  netplan set ethernets.eth1.addresses=["$ip4_addr"/24,"$ip6_addr"/64]
+  netplan apply
+else
+  ip -6 addr add "$ip6_addr"/64 dev eth1
+fi
+ip addr show dev eth1
+# Override default CNI and specify the interface since we don't have a default IPv6 route
+mkdir -p /var/lib/rancher/rke2/server/manifests
+
+case "$cni" in
+  "canal")
+    echo "Creating canal chart"
+    echo "apiVersion: helm.cattle.io/v1
+kind: HelmChartConfig
+metadata:
+  name: rke2-canal
+  namespace: kube-system
+spec:
+  valuesContent: |-
+    flannel:
+      iface: \"eth1\"
+    calico:
+      ipAutoDetectionMethod: \"interface=eth1.*\"
+      ip6AutoDetectionMethod: \"interface=eth1.*\"" >> /var/lib/rancher/rke2/server/manifests/e2e-canal.yaml
+  ;;
+  
+  "cilium")
+    echo "Creating cilium chart"
+    echo "apiVersion: helm.cattle.io/v1
+kind: HelmChartConfig
+metadata:
+  name: rke2-cilium
+  namespace: kube-system
+spec:
+  valuesContent: |-
+    devices: eth1
+    ipv6:
+      enabled: true">> /var/lib/rancher/rke2/server/manifests/e2e-cilium.yaml
+  ;;
+  
+  "calico")
+    echo "Creating calico chart"
+    echo "apiVersion: helm.cattle.io/v1
 kind: HelmChartConfig
 metadata:
   name: rke2-calico
@@ -33,3 +64,5 @@ spec:
           interface: eth1.* 
         nodeAddressAutodetectionV6:
           interface: eth1.* " >> /var/lib/rancher/rke2/server/manifests/e2e-calico.yaml
+  ;;
+esac
--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -1,11 +1,11 @@
 def defaultOSConfigure(vm)
 
   if vm.box.include?("ubuntu2004")
-    vm.provision "shell", inline: "systemd-resolve --set-dns=8.8.8.8 --interface=eth0", run: 'once'
-    vm.provision "shell", inline: "apt install -y jq", run: 'once'
+    vm.provision "netplan dns", type: "shell", inline: "netplan set ethernets.eth0.nameservers.addresses=[8.8.8.8,1.1.1.1]; netplan apply", run: 'once'
+    vm.provision "Install jq", type: "shell", inline: "apt install -y jq", run: 'once'
   end
   if vm.box.include?("Leap")
-    vm.provision "shell", inline: "zypper install -y jq", run: 'once'
+    vm.provision "Install jq", type: "shell", inline: "zypper install -y jq", run: 'once'
   end
   
 end
