83a07b9b97973abe6453f5aea9d70b1bd47dae93
Derek Nola <derek.nola@suse.com>
2022-05-24 11:04:11 -0700

    Add skip support
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -1,16 +1,13 @@
 def defaultOSConfigure(vm)
-
-  if vm.box.include?("ubuntu2004")
+  box = vm.box.to_s
+  if box.include?("generic/ubuntu")
     vm.provision "netplan dns", type: "shell", inline: "netplan set ethernets.eth0.nameservers.addresses=[8.8.8.8,1.1.1.1]; netplan apply", run: 'once'
     vm.provision "Install jq", type: "shell", inline: "apt-get install -y jq", run: 'once'
-  end
-  if vm.box.include?("Leap")
+  elsif box.include?("Leap") || box.include?("Tumbleweed")
     vm.provision "Install jq", type: "shell", inline: "zypper install -y jq", run: 'once'
-  end
-  if vm.box.match?(/windows.*2019/)
+  elsif box.match?(/windows.*2019/)
     vm.communicator = "winrm"
-  end
-  if vm.box.match?(/windows.*2022/)
+  elsif box.match?(/windows.*2022/)
     vm.communicator = "winrm"
   end
 end
