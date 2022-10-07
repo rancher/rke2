42bca0d534fe7b6a485e5ac6362d5913d27767b5
Derek Nola <derek.nola@suse.com>
2022-10-07 09:39:07 -0700

    Converted E2E CreateCluster to parallel
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/mixedos/mixedos_test.go
+++ tests/e2e/mixedos/mixedos_test.go
@@ -74,7 +74,7 @@ var _ = Describe("Verify Basic Cluster Creation", func() {
 	It("Starts up with no issues", func() {
 		var err error
 		serverNodeNames, linuxAgentNames, windowsAgentNames, err = createMixedCluster(*nodeOS, *serverCount, *linuxAgentCount, *windowsAgentCount)
-		Expect(err).NotTo(HaveOccurred(), e2e.GetVagrantLog())
+		Expect(err).NotTo(HaveOccurred(), e2e.GetVagrantLog(err))
 		fmt.Println("CLUSTER CONFIG")
 		fmt.Println("OS:", *nodeOS)
 		fmt.Println("Server Nodes:", serverNodeNames)
