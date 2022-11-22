b49642a05210e14ec2a9fbf2c6d09d15f8c8bdca
Brad Davidson <brad.davidson@rancher.com>
2022-11-22 00:39:55 +0000

    Update flags and component versions to track with K3s
    Bump golang/containerd/sonobuoy/golangci-lint versions
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>
    (cherry picked from commit e3225cd0ef819f22c55a98f3f0f62fcc0bb463b2)
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/windows/calico.go
+++ pkg/windows/calico.go
@@ -23,7 +23,6 @@ import (
 	wapi "github.com/iamacarpet/go-win64api"
 	"github.com/k3s-io/helm-controller/pkg/generated/controllers/helm.cattle.io"
 	util2 "github.com/k3s-io/k3s/pkg/agent/util"
-	"github.com/k3s-io/k3s/pkg/daemons/agent"
 	"github.com/k3s-io/k3s/pkg/daemons/config"
 	daemonconfig "github.com/k3s-io/k3s/pkg/daemons/config"
 	netroute "github.com/libp2p/go-netroute"
@@ -96,8 +95,6 @@ func (c *Calico) Setup(ctx context.Context, dataDir string, nodeConfig *daemonco
 	if err := getCNIConfigOverrides(cfg, hc); err != nil {
 		return nil, err
 	}
-
-	agent.NetworkName = CalicoHnsNetworkName
 
 	if err := createNodeNameFile(cfg, dataDir); err != nil {
 		return nil, err
