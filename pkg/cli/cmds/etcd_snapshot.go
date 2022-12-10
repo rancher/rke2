565a3cfa28f2fc4c0d523c006ce2d48c8c806c6b
Brad Davidson <brad.davidson@rancher.com>
2022-12-10 01:31:23 +0000

    Fix deprecated commands
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/cli/cmds/etcd_snapshot.go
+++ pkg/cli/cmds/etcd_snapshot.go
@@ -4,89 +4,62 @@ import (
 	"github.com/k3s-io/k3s/pkg/cli/cmds"
 	"github.com/k3s-io/k3s/pkg/cli/etcdsnapshot"
 	"github.com/k3s-io/k3s/pkg/configfilearg"
-	"github.com/rancher/rke2/pkg/rke2"
 	"github.com/urfave/cli"
 )
 
 const defaultSnapshotRentention = 5
 
-var k3sFlags = map[string]*K3SFlagOption{
-	"config":          copy,
-	"debug":           copy,
-	"log":             copy,
-	"alsologtostderr": copy,
-	"node-name":       copy,
-	"data-dir": {
-		Usage:   "(data) Folder to hold state",
-		Default: rke2Path,
-	},
-	"name":               copy,
-	"dir":                copy,
-	"snapshot-compress":  copy,
-	"s3":                 copy,
-	"s3-endpoint":        copy,
-	"s3-endpoint-ca":     copy,
-	"s3-skip-ssl-verify": copy,
-	"s3-access-key":      copy,
-	"s3-secret-key":      copy,
-	"s3-bucket":          copy,
-	"s3-region":          copy,
-	"s3-folder":          copy,
-	"s3-insecure":        copy,
-	"s3-timeout":         copy,
-}
-
-var subcommands = []cli.Command{
-	{
-		Name:            "delete",
-		Usage:           "Delete given snapshot(s)",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          etcdsnapshot.Delete,
-		Flags:           cmds.EtcdSnapshotFlags,
-	},
-	{
-		Name:            "ls",
-		Aliases:         []string{"list", "l"},
-		Usage:           "List snapshots",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          etcdsnapshot.List,
-		Flags:           cmds.EtcdSnapshotFlags,
-	},
-	{
-		Name:            "prune",
-		Usage:           "Remove snapshots that exceed the configured retention count",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          etcdsnapshot.Prune,
-		Flags: append(cmds.EtcdSnapshotFlags, &cli.IntFlag{
-			Name:        "snapshot-retention",
-			Usage:       "(db) Number of snapshots to retain. Default: 5",
-			Destination: &cmds.ServerConfig.EtcdSnapshotRetention,
-			Value:       defaultSnapshotRentention,
-		}),
-	},
-	{
-		Name:            "save",
-		Usage:           "Trigger an immediate etcd snapshot",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          etcdsnapshot.Run,
-		Flags:           cmds.EtcdSnapshotFlags,
-	},
-}
-
-var (
-	k3sEtcdSnapshotBase = mustCmdFromK3S(cmds.NewEtcdSnapshotCommand(EtcdSnapshotRun, subcommands), k3sFlags)
-)
-
 func NewEtcdSnapshotCommand() cli.Command {
-	cmd := k3sEtcdSnapshotBase
+	cmds.ServerConfig.DatastoreEndpoint = "etcd"
+	k3sOpts := K3SFlagSet{
+		"config":          copy,
+		"debug":           copy,
+		"log":             copy,
+		"alsologtostderr": copy,
+		"node-name":       copy,
+		"data-dir": {
+			Usage:   "(data) Folder to hold state",
+			Default: rke2Path,
+		},
+		"name":               copy,
+		"dir":                copy,
+		"snapshot-compress":  copy,
+		"s3":                 copy,
+		"s3-endpoint":        copy,
+		"s3-endpoint-ca":     copy,
+		"s3-skip-ssl-verify": copy,
+		"s3-access-key":      copy,
+		"s3-secret-key":      copy,
+		"s3-bucket":          copy,
+		"s3-region":          copy,
+		"s3-folder":          copy,
+		"s3-insecure":        copy,
+		"s3-timeout":         copy,
+	}
+	subcommandOpts := map[string]K3SFlagSet{
+		"ls": {
+			"output": copy,
+		},
+		"prune": {
+			"snapshot-retention": copy,
+		},
+	}
+
+	command := cmds.NewEtcdSnapshotCommands(
+		etcdsnapshot.Run,
+		etcdsnapshot.Delete,
+		etcdsnapshot.List,
+		etcdsnapshot.Prune,
+		etcdsnapshot.Save)
+	for i, subcommand := range command.Subcommands {
+		if s, ok := subcommandOpts[subcommand.Name]; ok {
+			k3sOpts.CopyInto(s)
+			command.Subcommands[i] = mustCmdFromK3S(subcommand, s)
+		} else {
+			command.Subcommands[i] = mustCmdFromK3S(subcommand, k3sOpts)
+		}
+	}
+	cmd := mustCmdFromK3S(command, k3sOpts)
 	configfilearg.DefaultParser.ValidFlags[cmd.Name] = cmd.Flags
 	return cmd
-}
-
-func EtcdSnapshotRun(clx *cli.Context) error {
-	return rke2.EtcdSnapshot(clx, config)
 }
--- pkg/cli/cmds/secrets_encrypt.go
+++ pkg/cli/cmds/secrets_encrypt.go
@@ -6,93 +6,45 @@ import (
 	"github.com/urfave/cli"
 )
 
-var secretsEncryptSubcommands = []cli.Command{
-	{
-		Name:            "status",
-		Usage:           "Print current status of secrets encryption",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Status,
-		Flags: append(cmds.EncryptFlags, &cli.StringFlag{
-			Name:        "output,o",
-			Usage:       "Status format. Default: text. Optional: json",
-			Destination: &cmds.ServerConfig.EncryptOutput,
-		}),
-	},
-	{
-		Name:            "enable",
-		Usage:           "Enable secrets encryption",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Enable,
-		Flags:           cmds.EncryptFlags,
-	},
-	{
-		Name:            "disable",
-		Usage:           "Disable secrets encryption",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Disable,
-		Flags:           cmds.EncryptFlags,
-	},
-	{
-		Name:            "prepare",
-		Usage:           "Prepare for encryption keys rotation",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Prepare,
-		Flags: append(cmds.EncryptFlags, &cli.BoolFlag{
-			Name:        "f,force",
-			Usage:       "Force preparation.",
-			Destination: &cmds.ServerConfig.EncryptForce,
-		}),
-	},
-	{
-		Name:            "rotate",
-		Usage:           "Rotate secrets encryption keys",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Rotate,
-		Flags: append(cmds.EncryptFlags, &cli.BoolFlag{
-			Name:        "f,force",
-			Usage:       "Force key rotation.",
-			Destination: &cmds.ServerConfig.EncryptForce,
-		}),
-	},
-	{
-		Name:            "reencrypt",
-		Usage:           "Reencrypt all data with new encryption key",
-		SkipFlagParsing: false,
-		SkipArgReorder:  true,
-		Action:          secretsencrypt.Reencrypt,
-		Flags: append(cmds.EncryptFlags,
-			&cli.BoolFlag{
-				Name:        "f, force",
-				Usage:       "Force secrets reencryption.",
-				Destination: &cmds.ServerConfig.EncryptForce,
-			},
-			&cli.BoolFlag{
-				Name:        "skip",
-				Usage:       "Skip removing old key",
-				Destination: &cmds.ServerConfig.EncryptSkip,
-			}),
-	},
-}
-
 func NewSecretsEncryptCommand() cli.Command {
-
-	var modifiedSubcommands []cli.Command
-	for _, subcommand := range secretsEncryptSubcommands {
-		modifiedSubcommands = append(modifiedSubcommands, mustCmdFromK3S(subcommand, map[string]*K3SFlagOption{
-			"data-dir": copy,
-			"token":    copy,
-			"server": {
-				Default: "https://127.0.0.1:9345",
-			},
-			"f":      ignore,
-			"skip":   ignore,
-			"output": ignore,
-		}))
+	k3sOpts := K3SFlagSet{
+		"data-dir": copy,
+		"token":    copy,
+		"server": {
+			Default: "https://127.0.0.1:9345",
+		},
 	}
-	return cmds.NewSecretsEncryptCommand(cli.ShowAppHelp, modifiedSubcommands)
+	subcommandOpts := map[string]K3SFlagSet{
+		"status": {
+			"output": copy,
+		},
+		"prepare": {
+			"force": copy,
+		},
+		"rotate": {
+			"force": copy,
+		},
+		"reencrypt": {
+			"force": copy,
+			"skip":  copy,
+		},
+	}
+
+	command := cmds.NewSecretsEncryptCommands(
+		secretsencrypt.Status,
+		secretsencrypt.Enable,
+		secretsencrypt.Disable,
+		secretsencrypt.Prepare,
+		secretsencrypt.Rotate,
+		secretsencrypt.Reencrypt)
+
+	for i, subcommand := range command.Subcommands {
+		if s, ok := subcommandOpts[subcommand.Name]; ok {
+			k3sOpts.CopyInto(s)
+			command.Subcommands[i] = mustCmdFromK3S(subcommand, s)
+		} else {
+			command.Subcommands[i] = mustCmdFromK3S(subcommand, k3sOpts)
+		}
+	}
+	return mustCmdFromK3S(command, nil)
 }
