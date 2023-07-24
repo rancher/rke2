rancher/rke2

commit b7b2e1a503907625a410e1a80e9a81a74a859add

install release from stable channel by default

install.sh
@@ -60,7 +60,7 @@ setup_env() {
 
     # --- make sure install channel has a value
     if [ -z "${INSTALL_RKE2_CHANNEL}" ]; then
-        INSTALL_RKE2_CHANNEL="latest"
+        INSTALL_RKE2_CHANNEL="stable"
     fi
 
     # --- make sure install type has a value

commit 0434da9a73db868e77a81b1d38edcbe28ceaa1b9

Fix incorrect documented default value for INSTALL_RKE2_CHANNEL
Fixes: b7b2e1a ("install release from stable channel by default")

install.sh
@@ -16,7 +16,7 @@ fi
 #
 #   - INSTALL_RKE2_CHANNEL
 #     Channel to use for fetching rke2 download URL.
-#     Defaults to 'latest'.
+#     Defaults to 'stable'.
 #
 #   - INSTALL_RKE2_METHOD
 #     The installation method to use.