3963d6bccbc76f225d0cd2f53610ccabff5d96df
Eric Ace <ericace@protonmail.com>
2023-07-31 16:22:48 -0400

    Uninstall handle cases when directories are mounts and cannot be removed (#4470)
    * Uninstall handle cases when directories are mounts and cannot be removed
    
    Co-authored-by: Eric Ace <esace@evoforge.org>

--- bundle/bin/rke2-uninstall.sh
+++ bundle/bin/rke2-uninstall.sh
@@ -96,7 +96,7 @@ uninstall_remove_files()
     rm -d /etc/rancher || true
     rm -rf /etc/cni
     rm -rf /opt/cni/bin
-    rm -rf /var/lib/kubelet
+    rm -rf /var/lib/kubelet || true
     rm -rf /var/lib/rancher/rke2
     rm -d /var/lib/rancher || true
 
