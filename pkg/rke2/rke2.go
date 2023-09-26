f51c662e8379dd3ba8876c4bbbb71efe90fa8a68
Derek Nola <derek.nola@suse.com>
2023-09-26 09:33:17 -0700

    [Release-1.25] Support generic "cis" profile (#4799)
    * Support generic "cis" profile
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    Co-authored-by: Brad Davidson <brad@oatmail.org>

--- pkg/rke2/rke2.go
+++ pkg/rke2/rke2.go
@@ -65,6 +65,7 @@ type ExtraEnv struct {
 // Valid CIS Profile versions
 const (
 	CISProfile123          = "cis-1.23"
+	CISProfile             = "cis"
 	defaultAuditPolicyFile = "/etc/rancher/rke2/audit-policy.yaml"
 	containerdSock         = "/run/k3s/containerd/containerd.sock"
 	KubeAPIServer          = "kube-apiserver"
@@ -275,7 +276,10 @@ func removeDisabledPods(dataDir, containerRuntimeEndpoint string, disabledItems 
 
 func isCISMode(clx *cli.Context) bool {
 	profile := clx.String("profile")
-	return profile == CISProfile123
+	if profile == CISProfile123 {
+		logrus.Warn("cis-1.23 profile is deprecated and will be removed in v1.29. Please use cis instead.")
+	}
+	return profile == CISProfile123 || profile == CISProfile
 }
 
 // TODO: move this into the podexecutor package, this logic is specific to that executor and should be there instead of here.
