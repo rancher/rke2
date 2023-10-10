59a81c1850a0fe95da1f14a19f34e11685b1fb4c
Derek Nola <derek.nola@suse.com>
2023-10-10 12:07:22 -0700

    Support new command functions
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- pkg/cli/cmds/token.go
+++ pkg/cli/cmds/token.go
@@ -25,9 +25,14 @@ func NewTokenCommand() cli.Command {
 		"list": {
 			"output": copyFlag,
 		},
+		"rotate": {
+			"token":     copyFlag,
+			"new-token": copyFlag,
+			"server":    copyFlag,
+		},
 	}
 
-	command := cmds.NewTokenCommands(token.Create, token.Delete, token.Generate, token.List)
+	command := cmds.NewTokenCommands(token.Create, token.Delete, token.Generate, token.List, token.Rotate)
 	configfilearg.DefaultParser.ValidFlags[command.Name] = command.Flags
 	for i, subcommand := range command.Subcommands {
 		if s, ok := subCommandOpts[subcommand.Name]; ok {
