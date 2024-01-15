170bd215aa73d92644cb65ecf61454ee4146561a
faelis <91593249+faelis@users.noreply.github.com>
2024-01-15 11:25:28 +0100

    fix: missing 'ip link delete cilium_wg0' in rke2-killall.sh

--- bundle/bin/rke2-killall.sh
+++ bundle/bin/rke2-killall.sh
@@ -78,6 +78,7 @@ ip link delete vxlan.calico
 ip link delete vxlan-v6.calico
 ip link delete cilium_vxlan
 ip link delete cilium_net
+ip link delete cilium_wg0
 ip link delete kube-ipvs0
 
 #Delete the nodeLocal created objects
