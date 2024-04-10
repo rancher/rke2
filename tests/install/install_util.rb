c292bb97b858ca5264e27c04dc756bc9b2e23f2e
Derek Nola <derek.nola@suse.com>
2024-04-10 11:09:32 -0700

    Rework and fix nightly install tests (#5692)
    * Remove microos install test
    * Rename ubuntu install test
    * Consolidate install testing checks
    * Move to linux GitHub Action runners
    * Bump leap to 15.5
    * Bump Windows 2019 to libvirt image
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Fix go version for integration tests
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    ---------
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

---
+++ tests/install/install_util.rb
@@ -0,0 +1,90 @@
+def waitForControlPlane(vm, box)
+  hostname = box.include?("opensuse") ? "$(hostnamectl --static)" : "$(hostname)"
+  vm.provision "rke2-wait-for-cp", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    echo 'Waiting for node (and static pods) to be ready ...'
+    time {
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready node/#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/etcd-#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/kube-apiserver-#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/kube-scheduler-#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/kube-proxy-#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/kube-controller-manager-#{hostname} 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl wait --for condition=ready -n kube-system pod/cloud-controller-manager-#{hostname} 2>/dev/null); do sleep 5; done'
+    }
+    kubectl get node,all -A -o wide
+    SHELL
+  end
+end
+
+
+def waitForCanal(vm)
+  vm.provision "rke2-wait-for-canal", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    time {
+        timeout 240 bash -c 'while ! (kubectl --namespace kube-system rollout status --timeout 10s daemonset/rke2-canal 2>/dev/null); do sleep 5; done'
+    }
+    SHELL
+  end
+end
+
+def waitForCoreDNS(vm)
+  vm.provision "rke2-wait-for-coredns", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    time {
+        timeout 240 bash -c 'while ! (kubectl --namespace kube-system rollout status --timeout 10s deploy/rke2-coredns-rke2-coredns 2>/dev/null); do sleep 5; done'
+        timeout 240 bash -c 'while ! (kubectl --namespace kube-system rollout status --timeout 10s deploy/rke2-coredns-rke2-coredns-autoscaler 2>/dev/null); do sleep 5; done'
+    }
+    SHELL
+  end
+end
+
+def waitForIngressNginx(vm)
+  vm.provision "rke2-wait-for-ingress-nginx", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    time {
+        timeout 240 bash -c 'while ! (kubectl --namespace kube-system rollout status --timeout 10s daemonset/rke2-ingress-nginx-controller 2>/dev/null); do sleep 5; done'
+    }
+    SHELL
+  end
+end
+
+def waitForMetricsServer(vm)
+  vm.provision "rke2-wait-for-metrics-server", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eu -o pipefail
+    time {
+        timeout 240 bash -c 'while ! (kubectl --namespace kube-system rollout status --timeout 10s deploy/rke2-metrics-server 2>/dev/null); do sleep 5; done'
+    }
+    SHELL
+  end
+end
+
+def checkRKE2Processes(vm)
+  vm.provision "rke2-procps", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eux -o pipefail
+    ps auxZ | grep -E 'etcd|kube|rke2|container|spc_t|unconfined_t' | grep -v grep
+    SHELL
+  end
+end
+
+def kubectlStatus(vm)
+  vm.provision "rke2-status", type: "shell", run: ENV['CI'] == 'true' ? 'never' : 'once' do |sh|
+    sh.inline = <<~SHELL
+    #!/usr/bin/env bash
+    set -eux -o pipefail
+    kubectl get node,all -A -o wide
+    SHELL
+  end
+end
