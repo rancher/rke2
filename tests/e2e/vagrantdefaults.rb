0328dbcb84a5951f5217563f99dcefade35d7ad7
Manuel Buil <mbuil@suse.com>
2024-05-10 09:41:43 +0200

    Replace deprecated exists by exist
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -17,7 +17,7 @@ def getInstallType(vm, version, branch)
     return "INSTALL_RKE2_VERSION=#{version}"
   end
   # Grabs the last 5 commit SHA's from the given branch, then purges any commits that do not have a passing CI build
-  scripts_location = Dir.exists?("./scripts") ? "./scripts" : "../scripts" 
+  scripts_location = Dir.exist?("./scripts") ? "./scripts" : "../scripts" 
   vm.provision "shell", path:  scripts_location + "/latest_commit.sh", args: [branch, "/tmp/rke2_commits"]
   return "INSTALL_RKE2_COMMIT=$(head\ -n\ 1\ /tmp/rke2_commits)"
 end
