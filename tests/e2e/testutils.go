2fcf6cd052d8ac4ef49b83cf65ccb0cd7b518b1d
Derek Nola <derek.nola@suse.com>
2024-06-05 13:03:31 -0700

    Support local windows builds in mixedOS e2e test
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/testutils.go
+++ tests/e2e/testutils.go
@@ -79,7 +79,7 @@ func genNodeEnvs(nodeOS string, serverCount, agentCount, windowsAgentCount int) 
 	for i := 0; i < serverCount; i++ {
 		serverNodeNames[i] = "server-" + strconv.Itoa(i)
 	}
-	agentPrefix := ""
+	var agentPrefix string
 	if windowsAgentCount > 0 {
 		agentPrefix = "linux-"
 	}
@@ -205,7 +205,6 @@ func CreateLocalCluster(nodeOS string, serverCount, agentCount int) ([]string, [
 	serverNodeNames, agentNodeNames, _, nodeEnvs := genNodeEnvs(nodeOS, serverCount, agentCount, 0)
 
 	var testOptions string
-	var cmd string
 
 	for _, env := range os.Environ() {
 		if strings.HasPrefix(env, "E2E_") {
@@ -216,7 +215,7 @@ func CreateLocalCluster(nodeOS string, serverCount, agentCount int) ([]string, [
 
 	// Provision the first server node. In GitHub Actions, this also imports the VM image into libvirt, which
 	// takes time and can cause the next vagrant up to fail if it is not given enough time to complete.
-	cmd = fmt.Sprintf(`%s %s vagrant up --no-provision %s &> vagrant.log`, nodeEnvs, testOptions, serverNodeNames[0])
+	cmd := fmt.Sprintf(`%s %s vagrant up --no-provision %s &> vagrant.log`, nodeEnvs, testOptions, serverNodeNames[0])
 	fmt.Println(cmd)
 	if _, err := RunCommand(cmd); err != nil {
 		return nil, nil, newNodeError(cmd, serverNodeNames[0], err)
@@ -259,6 +258,86 @@ func CreateLocalCluster(nodeOS string, serverCount, agentCount int) ([]string, [
 		return nil, nil, err
 	}
 	return serverNodeNames, agentNodeNames, nil
+}
+
+func scpWindowsRKE2Artifacts(nodeNames []string) error {
+	binary := []string{
+		"dist/artifacts/rke2.windows-amd64.tar.gz",
+		"dist/artifacts/sha256sum-windows-amd64.txt",
+	}
+	images := []string{
+		"build/images/rke2-images.windows-amd64.tar.zst",
+	}
+
+	// vagrant scp doesn't allow coping multiple files at once
+	// nor does it allow copying as sudo, so we have to copy each file individually
+	// to /tmp/ and then move them to the correct location
+	for _, node := range nodeNames {
+		for _, artifact := range append(binary, images...) {
+			cmd := fmt.Sprintf(`vagrant scp ../../../%s %s:/tmp/`, artifact, node)
+			if _, err := RunCommand(cmd); err != nil {
+				return err
+			}
+		}
+		if _, err := RunCmdOnWindowsNode(`mv C:\tmp\sha256sum-windows-amd64.txt C:\tmp\sha256sum-amd64.txt`, node); err != nil {
+			return err
+		}
+		if _, err := RunCmdOnWindowsNode(`mkdir C:\var\lib\rancher\rke2\agent\images`, node); err != nil {
+			return err
+		}
+		for _, image := range images {
+			cmd := fmt.Sprintf("mv C:\\tmp\\%s C:\\var\\lib\\rancher\\rke2\\agent\\images\\", filepath.Base(image))
+			if _, err := RunCmdOnNode(cmd, node); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func CreateLocalMixedCluster(nodeOS string, serverCount, linuxAgentCount, windowsAgentCount int) ([]string, []string, []string, error) {
+	serverNodeNames, linuxAgentNames, windowsAgentNames, nodeEnvs := genNodeEnvs(nodeOS, serverCount, linuxAgentCount, windowsAgentCount)
+
+	var testOptions string
+	for _, env := range os.Environ() {
+		if strings.HasPrefix(env, "E2E_") {
+			testOptions += " " + env
+		}
+	}
+	testOptions += " E2E_RELEASE_VERSION=skip"
+
+	// Standup all nodes, relying on vagrant-libvirt native parallel provisioning
+	cmd := fmt.Sprintf(`%s %s E2E_STANDUP_PARALLEL=true vagrant up --no-provision &> vagrant.log`, nodeEnvs, testOptions)
+	fmt.Println(cmd)
+	if _, err := RunCommand(cmd); err != nil {
+		return nil, nil, nil, err
+	}
+
+	if err := scpRKE2Artifacts(append(serverNodeNames, linuxAgentNames...)); err != nil {
+		return nil, nil, nil, err
+	}
+	if err := scpWindowsRKE2Artifacts(windowsAgentNames); err != nil {
+		return nil, nil, nil, err
+	}
+	// Install RKE2 on all nodes in parallel
+	errg, _ := errgroup.WithContext(context.Background())
+	allNodes := append(serverNodeNames, linuxAgentNames...)
+	allNodes = append(allNodes, windowsAgentNames...)
+	for _, node := range allNodes {
+		cmd = fmt.Sprintf(`%s %s vagrant provision %s &>> vagrant.log`, nodeEnvs, testOptions, node)
+		errg.Go(func() error {
+			if _, err := RunCommand(cmd); err != nil {
+				return newNodeError(cmd, node, err)
+			}
+			return nil
+		})
+		// RKE2 needs some time between joining nodes to avoid learner issues
+		time.Sleep(20 * time.Second)
+	}
+	if err := errg.Wait(); err != nil {
+		return nil, nil, nil, err
+	}
+	return serverNodeNames, linuxAgentNames, windowsAgentNames, nil
 }
 
 func DeployWorkload(workload string, kubeconfig string) (string, error) {
