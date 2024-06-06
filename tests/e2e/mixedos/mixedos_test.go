4c7c744996b0a927ba5a1f8e459bf0cf027a748e
Derek Nola <derek.nola@suse.com>
2024-06-06 14:51:49 -0700

    Support MixedOS E2E local testing (#6137)
    * Consolidate CreateWindowsCluster test function
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Support local windows builds in mixedOS e2e test
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Add mixedos to E2E CI
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Simplify local cluster provisioning with libvirt native parallel bringup
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    * Don't run mixedOS on E2E
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>
    
    ---------
    
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/mixedos/mixedos_test.go
+++ tests/e2e/mixedos/mixedos_test.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"testing"
 
@@ -16,52 +15,16 @@ import (
 // Valid nodeOS: generic/ubuntu2004, opensuse/Leap-15.3.x86_64
 var nodeOS = flag.String("nodeOS", "generic/ubuntu2004", "operating system for linux nodes")
 var serverCount = flag.Int("serverCount", 1, "number of server nodes")
-var linuxAgentCount = flag.Int("linuxAgentCount", 1, "number of linux agent nodes")
+var linuxAgentCount = flag.Int("linuxAgentCount", 0, "number of linux agent nodes")
 var windowsAgentCount = flag.Int("windowsAgentCount", 1, "number of windows agent nodes")
 var ci = flag.Bool("ci", false, "running on CI")
-
-const defaultWindowsOS = "jborean93/WindowsServer2022"
+var local = flag.Bool("local", false, "deploy a locally built RKE2")
 
 func Test_E2EMixedOSValidation(t *testing.T) {
 	flag.Parse()
 	RegisterFailHandler(Fail)
 	suiteConfig, reporterConfig := GinkgoConfiguration()
 	RunSpecs(t, "Validate MixedOS Test Suite", suiteConfig, reporterConfig)
-}
-
-func createMixedCluster(nodeOS string, serverCount, linuxAgentCount, windowsAgentCount int) ([]string, []string, []string, error) {
-	serverNodeNames := []string{}
-	for i := 0; i < serverCount; i++ {
-		serverNodeNames = append(serverNodeNames, "server-"+strconv.Itoa(i))
-	}
-	linuxAgentNames := []string{}
-	for i := 0; i < linuxAgentCount; i++ {
-		linuxAgentNames = append(linuxAgentNames, "linux-agent-"+strconv.Itoa(i))
-	}
-	windowsAgentNames := []string{}
-	for i := 0; i < linuxAgentCount; i++ {
-		windowsAgentNames = append(windowsAgentNames, "windows-agent-"+strconv.Itoa(i))
-	}
-	nodeRoles := strings.Join(serverNodeNames, " ") + " " + strings.Join(linuxAgentNames, " ") + " " + strings.Join(windowsAgentNames, " ")
-	nodeRoles = strings.TrimSpace(nodeRoles)
-	nodeBoxes := strings.Repeat(nodeOS+" ", serverCount+linuxAgentCount)
-	nodeBoxes += strings.Repeat(defaultWindowsOS+" ", windowsAgentCount)
-	nodeBoxes = strings.TrimSpace(nodeBoxes)
-
-	var testOptions string
-	for _, env := range os.Environ() {
-		if strings.HasPrefix(env, "E2E_") {
-			testOptions += " " + env
-		}
-	}
-
-	cmd := fmt.Sprintf("NODE_ROLES=\"%s\" NODE_BOXES=\"%s\" %s vagrant up &> vagrant.log", nodeRoles, nodeBoxes, testOptions)
-	fmt.Println(cmd)
-	if _, err := e2e.RunCommand(cmd); err != nil {
-		fmt.Println("Error Creating Cluster", err)
-		return nil, nil, nil, err
-	}
-	return serverNodeNames, linuxAgentNames, windowsAgentNames, nil
 }
 
 var (
@@ -76,7 +39,11 @@ var _ = Describe("Verify Basic Cluster Creation", Ordered, func() {
 
 	It("Starts up with no issues", func() {
 		var err error
-		serverNodeNames, linuxAgentNames, windowsAgentNames, err = createMixedCluster(*nodeOS, *serverCount, *linuxAgentCount, *windowsAgentCount)
+		if *local {
+			serverNodeNames, linuxAgentNames, windowsAgentNames, err = e2e.CreateLocalMixedCluster(*nodeOS, *serverCount, *linuxAgentCount, *windowsAgentCount)
+		} else {
+			serverNodeNames, linuxAgentNames, windowsAgentNames, err = e2e.CreateMixedCluster(*nodeOS, *serverCount, *linuxAgentCount, *windowsAgentCount)
+		}
 		Expect(err).NotTo(HaveOccurred(), e2e.GetVagrantLog(err))
 		fmt.Println("CLUSTER CONFIG")
 		fmt.Println("OS:", *nodeOS)
