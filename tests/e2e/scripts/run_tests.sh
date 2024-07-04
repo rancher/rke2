fd60c9b1640c75b9981d3241d92e3904d4020ac8
Manuel Buil <mbuil@suse.com>
2024-07-04 11:17:16 +0200

    Improve run_tests script
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- tests/e2e/scripts/run_tests.sh
+++ tests/e2e/scripts/run_tests.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-nodeOS=${1:-"generic/ubuntu2004"}
-servercount=${2:-3}
-agentcount=${3:-1}
-db=${4:-"etcd"}
-hardened=${5:-""}
-rke2_version=${rke2_version}
-rke2_channel=${rke2_channel:-"commit"}
+# Usage: ./run_tests.sh
+# This script runs all the rke2 e2e tests and generates a report with the log
+# The generated log is placed in createreport/rke2_${date}.log
+#
+# This script must be run inside the rke2 directory where the tests exist
+#
+# Example:
+#   To run the script with default settings:
+#     ./run_tests.sh
+#
+set -x
 
-E2E_EXTERNAL_DB=$db && export E2E_EXTERNAL_DB
+# tests to run
+tests=("ciliumnokp" "dnscache" "dualstack" "mixedos" "mixedosbgp" "multus" "secretsencryption" "splitserver" "upgradecluster" "validatecluster")
+date=$(date +%Y%m%d)
+
 E2E_REGISTRY=true && export E2E_REGISTRY
 
-cd
-cd rke2 && git pull --rebase origin master
+git pull --rebase origin master
 /usr/local/go/bin/go mod tidy
-
 cd tests/e2e
-OS=$(echo "$nodeOS"|cut -d'/' -f2)
-echo "$OS"
 
 # create directory to store reports if it does not exists
 if [ ! -d createreport ]
@@ -24,39 +27,34 @@ then
 	mkdir createreport
 fi
 
+# Remove VMs which are in invalid state
+vagrant global-status --prune
+
 count=0
 run_tests(){
-	count=$(( count + 1 ))
-	vagrant global-status | awk '/running/'|cut -c1-7| xargs -r -d '\n' -n 1 -- vagrant destroy -f
 
-	E2E_RELEASE_VERSION=$rke2_version && export E2E_RELEASE_VERSION
-	E2E_RELEASE_CHANNEL=$rke2_channel && export E2E_RELEASE_CHANNEL
+    count=$(( count + 1 ))
 
-	echo 'RUNNING CLUSTER UPGRADE TEST'
-	E2E_REGISTRY=true /usr/local/go/bin/go test -v ./upgradecluster/upgradecluster_test.go -nodeOS="$nodeOS" -serverCount=$((servercount)) -agentCount=$((agentcount)) -timeout=1h -json -ci |tee createreport/rke2_"$OS".log
+    for i in ${!tests[@]}; do
+	pushd ${tests[$i]}
+	vagrant destroy -f
 
-	echo 'RUNNING DUALSTACK VALIDATION TEST'
-	E2E_HARDENED="$hardened" /usr/local/go/bin/go test -v dualstack/dualstack_test.go -nodeOS="$nodeOS" -serverCount=1 -agentCount=1  -timeout=30m -json -ci |tee -a createreport/rke2_"$OS".log
-
-	echo 'RUNNING CLUSTER VALIDATION TEST'
-	E2E_REGISTRY=true E2E_HARDENED="$hardened" /usr/local/go/bin/go test -v validatecluster/validatecluster_test.go -nodeOS="$nodeOS" -serverCount=$((servercount)) -agentCount=$((agentcount))  -timeout=30m -json -ci |tee -a createreport/rke2_"$OS".log
-
-	echo 'RUNNING MIXEDOS TEST'
-	/usr/local/go/bin/go test -v mixedos/mixedos_test.go -nodeOS="$nodeOS" -serverCount=$((servercount)) -timeout=1h -json -ci |tee -a  createreport/rke2_"$OS".log
-
-	echo 'RUNNING SPLIT SERVER VALIDATION TEST'
-	E2E_HARDENED="$hardened" /usr/local/go/bin/go test -v splitserver/splitserver_test.go -nodeOS="$nodeOS" -timeout=30m -json -ci |tee -a createreport/rke2_"$OS".log
+        echo "RUNNING ${tests[$i]} TEST"
+        /usr/local/go/bin/go test -v ${tests[$i]}_test.go -timeout=1h -json -ci |tee -a ../createreport/rke2_${date}.log
+        
+	popd
+    done
 }
 
-ls createreport/rke2_"$OS".log 2>/dev/null && rm createreport/rke2_"$OS".log
+ls createreport/rke2_${date}.log 2>/dev/null && rm createreport/rke2_${date}.log
 run_tests
 
 # re-run test if first run fails and keep record of repeatedly failed test to debug
-while [ -f createreport/rke2_"$OS".log ] && grep -w ":fail" createreport/rke2_"$OS".log && [ $count -le 2 ]
+while [ -f createreport/rke2_${date}.log ] && grep -w " FAIL:" createreport/rke2_${date}.log && [ $count -le 2 ]
 do
-        cp createreport/rke2_"$OS".log createreport/rke2_"$OS"_"$count".log
+        cp createreport/rke2_${date}.log createreport/rke2_${date}_${count}.log
         run_tests
 done
 
 # Generate report and upload to s3 bucket
-cd createreport && /usr/local/go/bin/go run -v report-template-bindata.go generate_report.go -f rke2_"OS".log
+cd createreport && /usr/local/go/bin/go run -v report-template-bindata.go generate_report.go -f rke2_${date}.log
