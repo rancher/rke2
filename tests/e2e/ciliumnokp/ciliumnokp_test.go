17c5f369f98230d65a1ff7ef0b30a583173e0e65
manuelbuil <mbuil@suse.com>
2024-09-12 19:05:46 +0200

    Update cilium e2e test
    Signed-off-by: manuelbuil <mbuil@suse.com>

--- tests/e2e/ciliumnokp/ciliumnokp_test.go
+++ tests/e2e/ciliumnokp/ciliumnokp_test.go
@@ -96,13 +96,13 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 			res, err := e2e.RunCommand(cmd)
 			Expect(err).NotTo(HaveOccurred(), res)
 			// We expect the following output and the important parts are HostPort, LoadBalancer, Host Routing and Masquerading
-			// Host Routing:           BPF
+			// Routing:                Network: Tunnel [vxlan]   Host: BPF
 			// Masquerading:           BPF
 			// Clock Source for BPF:   ktime
 			// - LoadBalancer:   Enabled
 			// - HostPort:       Enabled
 			// BPF Maps:   dynamic sizing: on (ratio: 0.002500)
-			Expect(res).Should(ContainSubstring("Host Routing"))
+			Expect(res).Should(ContainSubstring("Routing"))
 			Expect(res).Should(ContainSubstring("Masquerading"))
 			Expect(res).Should(ContainSubstring("LoadBalancer:   Enabled"))
 			Expect(res).Should(ContainSubstring("HostPort:       Enabled"))
