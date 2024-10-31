cb4adfab3cddd5ea083021b8c2898dc65876342c
Vitor Savian <vitor.savian@suse.com>
2024-10-31 12:15:57 -0300

    Add tests for kine
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- tests/e2e/vagrantdefaults.rb
+++ tests/e2e/vagrantdefaults.rb
@@ -36,3 +36,25 @@ def loadManifests(vm, files)
   end
   vm.provision "Deploy additional manifests", type: "shell", inline: "mv /tmp/manifests /var/lib/rancher/rke2/server/manifests"
 end
+
+def dockerInstall(vm)
+  vm.provider "libvirt" do |v|
+    v.memory = NODE_MEMORY + 1024
+  end
+  vm.provider "virtualbox" do |v|
+    v.memory = NODE_MEMORY + 1024
+  end
+  box = vm.box.to_s
+  if box.include?("ubuntu")
+    vm.provision "shell", inline: "apt update; apt install -y docker.io"
+  elsif box.include?("Leap")
+    vm.provision "shell", inline: "zypper install -y docker apparmor-parser"
+  elsif box.include?("microos")
+    vm.provision "shell", inline: "transactional-update pkg install -y docker apparmor-parser"
+    vm.provision 'docker-reload', type: 'reload', run: 'once'
+    vm.provision "shell", inline: "systemctl enable --now docker"
+  elsif box.include?("rocky")
+    vm.provision "shell", inline: "dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+    vm.provision "shell", inline: "dnf install -y docker-ce"
+  end
+end
