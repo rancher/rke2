3daa21a6469244b4dc30546f3627b84ac6306f5f
Derek Nola <derek.nola@suse.com>
2025-02-28 12:13:52 -0800

    Convert E2E testutils to TestConfig and VagrantNode structs
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/ciliumnokp/ciliumnokp_test.go
+++ tests/e2e/ciliumnokp/ciliumnokp_test.go
@@ -26,11 +26,7 @@ func Test_E2ECiliumNoKP(t *testing.T) {
 	RunSpecs(t, "Validate dualstack in Cilium without kube-proxy Test Suite", suiteConfig, reporterConfig)
 }
 
-var (
-	kubeConfigFile  string
-	serverNodeNames []string
-	agentNodeNames  []string
-)
+var tc *e2e.TestConfig
 var _ = ReportAfterEach(e2e.GenReport)
 
 var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", Ordered, func() {
@@ -38,34 +34,33 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	It("Starts up with no issues", func() {
 		var err error
 		if *local {
-			serverNodeNames, agentNodeNames, err = e2e.CreateLocalCluster(*nodeOS, *serverCount, *agentCount)
+			tc, err = e2e.CreateLocalCluster(*nodeOS, *serverCount, *agentCount)
 		} else {
-			serverNodeNames, agentNodeNames, err = e2e.CreateCluster(*nodeOS, *serverCount, *agentCount)
+			tc, err = e2e.CreateCluster(*nodeOS, *serverCount, *agentCount)
 		}
 		Expect(err).NotTo(HaveOccurred(), e2e.GetVagrantLog(err))
-		fmt.Println("CLUSTER CONFIG")
-		fmt.Println("OS:", *nodeOS)
-		fmt.Println("Server Nodes:", serverNodeNames)
-		fmt.Println("Agent Nodes:", agentNodeNames)
-		kubeConfigFile, err = e2e.GenKubeConfigFile(serverNodeNames[0])
+		By("CLUSTER CONFIG")
+		By("OS: " + *nodeOS)
+		By(tc.Status())
+		tc.KubeconfigFile, err = e2e.GenKubeConfigFile(tc.Servers[0])
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("Checks Node Status", func() {
 		Eventually(func(g Gomega) {
-			nodes, err := e2e.ParseNodes(kubeConfigFile, false)
+			nodes, err := e2e.ParseNodes(tc.KubeconfigFile, false)
 			g.Expect(err).NotTo(HaveOccurred())
 			for _, node := range nodes {
 				g.Expect(node.Status).Should(Equal("Ready"))
 			}
 		}, "620s", "5s").Should(Succeed())
-		_, err := e2e.ParseNodes(kubeConfigFile, true)
+		_, err := e2e.ParseNodes(tc.KubeconfigFile, true)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("Checks Pod Status", func() {
 		Eventually(func(g Gomega) {
-			pods, err := e2e.ParsePods(kubeConfigFile, false)
+			pods, err := e2e.ParsePods(tc.KubeconfigFile, false)
 			g.Expect(err).NotTo(HaveOccurred())
 			for _, pod := range pods {
 				if strings.Contains(pod.Name, "helm-install") {
@@ -75,14 +70,14 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 				}
 			}
 		}, "420s", "5s").Should(Succeed())
-		_, err := e2e.ParsePods(kubeConfigFile, true)
+		_, err := e2e.ParsePods(tc.KubeconfigFile, true)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("Verifies that each node has IPv4 and IPv6", func() {
-		for _, node := range serverNodeNames {
+		for _, node := range tc.Servers {
 			cmd := fmt.Sprintf("kubectl get node %s -o jsonpath='{.status.addresses}' --kubeconfig=%s | jq '.[] | select(.type == \"ExternalIP\") | .address'",
-				node, kubeConfigFile)
+				node.Name, tc.KubeconfigFile)
 			res, err := e2e.RunCommand(cmd)
 			Expect(err).NotTo(HaveOccurred(), res)
 			Expect(res).Should(ContainSubstring("10.10.10"))
@@ -91,13 +86,13 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	})
 
 	It("Verifies that cilium config is correct", func() {
-		cmdCiliumAgents := "kubectl get pods -l app.kubernetes.io/name=cilium-agent -n kube-system -o=name --kubeconfig=" + kubeConfigFile
+		cmdCiliumAgents := "kubectl get pods -l app.kubernetes.io/name=cilium-agent -n kube-system -o=name --kubeconfig=" + tc.KubeconfigFile
 		res, err := e2e.RunCommand(cmdCiliumAgents)
 		Expect(err).NotTo(HaveOccurred(), res)
 		ciliumAgents := strings.Split(strings.TrimSpace(res), "\n")
-		Expect(len(ciliumAgents)).Should(Equal(len(serverNodeNames) + len(agentNodeNames)))
+		Expect(len(ciliumAgents)).Should(Equal(len(tc.Servers) + len(tc.Agents)))
 		for _, ciliumAgent := range ciliumAgents {
-			cmd := "kubectl exec " + ciliumAgent + " -n kube-system  -c cilium-agent --kubeconfig=" + kubeConfigFile + " -- cilium-dbg status --verbose | grep -e 'BPF' -e 'HostPort' -e 'LoadBalancer'"
+			cmd := "kubectl exec " + ciliumAgent + " -n kube-system  -c cilium-agent --kubeconfig=" + tc.KubeconfigFile + " -- cilium-dbg status --verbose | grep -e 'BPF' -e 'HostPort' -e 'LoadBalancer'"
 			res, err := e2e.RunCommand(cmd)
 			Expect(err).NotTo(HaveOccurred(), res)
 			// We expect the following output and the important parts are HostPort, LoadBalancer, Host Routing and Masquerading
@@ -115,21 +110,21 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	})
 
 	It("Verifies ClusterIP Service", func() {
-		_, err := e2e.DeployWorkload("dualstack_clusterip.yaml", kubeConfigFile)
+		_, err := tc.DeployWorkload("dualstack_clusterip.yaml")
 		Expect(err).NotTo(HaveOccurred())
 		Eventually(func() (string, error) {
-			cmd := "kubectl get pods -o=name -l k8s-app=nginx-app-clusterip --field-selector=status.phase=Running --kubeconfig=" + kubeConfigFile
+			cmd := "kubectl get pods -o=name -l k8s-app=nginx-app-clusterip --field-selector=status.phase=Running --kubeconfig=" + tc.KubeconfigFile
 			return e2e.RunCommand(cmd)
 		}, "120s", "5s").Should(ContainSubstring("ds-clusterip-pod"))
 
 		// Checks both IPv4 and IPv6
-		clusterips, err := e2e.FetchClusterIP(kubeConfigFile, "ds-clusterip-svc", true)
+		clusterips, err := e2e.FetchClusterIP(tc.KubeconfigFile, "ds-clusterip-svc", true)
 		Expect(err).NotTo(HaveOccurred())
 		for _, ip := range strings.Split(clusterips, ",") {
 			if strings.Contains(ip, "::") {
 				ip = "[" + ip + "]"
 			}
-			pods, err := e2e.ParsePods(kubeConfigFile, false)
+			pods, err := e2e.ParsePods(tc.KubeconfigFile, false)
 			Expect(err).NotTo(HaveOccurred())
 			for _, pod := range pods {
 				if !strings.HasPrefix(pod.Name, "ds-clusterip-pod") {
@@ -137,26 +132,26 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 				}
 				cmd := fmt.Sprintf("curl -L --insecure http://%s", ip)
 				Eventually(func() (string, error) {
-					return e2e.RunCmdOnNode(cmd, serverNodeNames[0])
+					return tc.Servers[0].RunCmdOnNode(cmd)
 				}, "60s", "5s").Should(ContainSubstring("Welcome to nginx!"), "failed cmd: "+cmd)
 			}
 		}
 	})
 
 	It("Verifies internode connectivity", func() {
-		_, err := e2e.DeployWorkload("pod_client.yaml", kubeConfigFile)
+		_, err := tc.DeployWorkload("pod_client.yaml")
 		Expect(err).NotTo(HaveOccurred())
 
 		// Wait for the pod_client to have an IP
 		Eventually(func() string {
-			ips, _ := e2e.PodIPsUsingLabel(kubeConfigFile, "app=client")
+			ips, _ := e2e.PodIPsUsingLabel(tc.KubeconfigFile, "app=client")
 			return ips[0].Ipv4
 		}, "40s", "5s").Should(ContainSubstring("10.42"), "failed getClientIPs")
 
-		clientIPs, err := e2e.PodIPsUsingLabel(kubeConfigFile, "app=client")
+		clientIPs, err := e2e.PodIPsUsingLabel(tc.KubeconfigFile, "app=client")
 		Expect(err).NotTo(HaveOccurred())
 		for _, ip := range clientIPs {
-			cmd := "kubectl exec svc/client-curl --kubeconfig=" + kubeConfigFile + " -- curl -m7 " + ip.Ipv4 + "/name.html"
+			cmd := "kubectl exec svc/client-curl --kubeconfig=" + tc.KubeconfigFile + " -- curl -m7 " + ip.Ipv4 + "/name.html"
 			Eventually(func() (string, error) {
 				return e2e.RunCommand(cmd)
 			}, "20s", "3s").Should(ContainSubstring("client-deployment"), "failed cmd: "+cmd)
@@ -164,12 +159,12 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	})
 
 	It("Verifies Ingress", func() {
-		_, err := e2e.DeployWorkload("dualstack_ingress.yaml", kubeConfigFile)
+		_, err := tc.DeployWorkload("dualstack_ingress.yaml")
 		Expect(err).NotTo(HaveOccurred(), "Ingress manifest not deployed")
-		cmd := "kubectl get ingress ds-ingress --kubeconfig=" + kubeConfigFile + " -o jsonpath=\"{.spec.rules[*].host}\""
+		cmd := "kubectl get ingress ds-ingress --kubeconfig=" + tc.KubeconfigFile + " -o jsonpath=\"{.spec.rules[*].host}\""
 		hostName, err := e2e.RunCommand(cmd)
 		Expect(err).NotTo(HaveOccurred(), "failed cmd: "+cmd)
-		nodeIPs, err := e2e.GetNodeIPs(kubeConfigFile)
+		nodeIPs, err := e2e.GetNodeIPs(tc.KubeconfigFile)
 		Expect(err).NotTo(HaveOccurred(), "failed cmd: "+cmd)
 		for _, node := range nodeIPs {
 			cmd := fmt.Sprintf("curl  --header host:%s http://%s/name.html", hostName, node.Ipv4)
@@ -184,12 +179,12 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	})
 
 	It("Verifies NodePort Service", func() {
-		_, err := e2e.DeployWorkload("dualstack_nodeport.yaml", kubeConfigFile)
+		_, err := tc.DeployWorkload("dualstack_nodeport.yaml")
 		Expect(err).NotTo(HaveOccurred())
-		cmd := "kubectl get service ds-nodeport-svc --kubeconfig=" + kubeConfigFile + " --output jsonpath=\"{.spec.ports[0].nodePort}\""
+		cmd := "kubectl get service ds-nodeport-svc --kubeconfig=" + tc.KubeconfigFile + " --output jsonpath=\"{.spec.ports[0].nodePort}\""
 		nodeport, err := e2e.RunCommand(cmd)
 		Expect(err).NotTo(HaveOccurred(), "failed cmd: "+cmd)
-		nodeIPs, err := e2e.GetNodeIPs(kubeConfigFile)
+		nodeIPs, err := e2e.GetNodeIPs(tc.KubeconfigFile)
 		Expect(err).NotTo(HaveOccurred())
 		for _, node := range nodeIPs {
 			cmd = "curl -L --insecure http://" + node.Ipv4 + ":" + nodeport + "/name.html"
@@ -206,8 +201,8 @@ var _ = Describe("Verify DualStack in Cilium without kube-proxy configuration", 
 	It("Verifies there are no required iptables", func() {
 		// Check that there are no iptables rules with KUBE-SVC and HOSTPORT
 		cmdiptables := "sudo iptables-save | grep -e 'KUBE-SVC' -e 'HOSTPORT' | wc -l"
-		for i := range serverNodeNames {
-			res, err := e2e.RunCmdOnNode(cmdiptables, serverNodeNames[i])
+		for _, server := range tc.Servers {
+			res, err := server.RunCmdOnNode(cmdiptables)
 			Expect(err).NotTo(HaveOccurred(), res)
 			Expect(res).Should(ContainSubstring("0"))
 		}
@@ -225,6 +220,6 @@ var _ = AfterSuite(func() {
 		fmt.Println("FAILED!")
 	} else {
 		Expect(e2e.DestroyCluster()).To(Succeed())
-		Expect(os.Remove(kubeConfigFile)).To(Succeed())
+		Expect(os.Remove(tc.KubeconfigFile)).To(Succeed())
 	}
 })
