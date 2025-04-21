2e939d1c75451445920e8ecf725280f54d2d566d
Brad Davidson <brad.davidson@rancher.com>
2025-04-21 19:36:36 +0000

    Bump traefik to v2.11.24
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- charts/build-charts.sh
+++ charts/build-charts.sh
@@ -3,6 +3,7 @@
 set -eux -o pipefail
 CHARTS_DIR=$(dirname $0)
 
-while read version filename bootstrap; do
+while read version filename bootstrap package; do
+  if [ -n "$package" ]; then export CHART_PACKAGE="${package}"; else unset CHART_PACKAGE; fi
   CHART_VERSION=$version CHART_FILE=$CHARTS_DIR/$(basename $filename) CHART_BOOTSTRAP=$bootstrap $CHARTS_DIR/build-chart.sh
-done <<< $(yq e '.charts[] | [.version, .filename, .bootstrap] | join(" ")' $CHARTS_DIR/chart_versions.yaml)
+done <<< $(yq e '.charts[] | [.version, .filename, .bootstrap, .package] | join(" ")' $CHARTS_DIR/chart_versions.yaml)
