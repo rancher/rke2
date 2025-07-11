c5d013843b40d73e5f58a5767439869e95a6121a
Vitor Savian <vitor.savian@suse.com>
2025-07-11 04:21:56 -0300

    Update shell completion command to new structure
    Signed-off-by: Vitor Savian <vitor.savian@suse.com>

--- pkg/cli/cmds/completion.go
+++ pkg/cli/cmds/completion.go
@@ -10,6 +10,49 @@ import (
 )
 
 var (
+	bashScript = `#! /bin/bash
+_cli_bash_autocomplete() {
+if [[ "${COMP_WORDS[0]}" != "source" ]]; then
+	local cur opts base
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	if [[ "$cur" == "-"* ]]; then
+	opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+	else
+	opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+	fi
+	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	return 0
+fi
+}
+
+complete -o bashdefault -o default -o nospace -F _cli_bash_autocomplete %s
+`
+
+	zshScript = `#compdef %[1]s
+_cli_zsh_autocomplete() {
+
+	local -a opts
+	local cur
+	cur=${words[-1]}
+	if [[ "$cur" == "-"* ]]; then
+	opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
+	else
+	opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
+	fi
+
+	if [[ "${opts[1]}" != "" ]]; then
+	_describe 'values' opts
+	else
+	_files
+	fi
+
+	return
+}
+
+compdef _cli_zsh_autocomplete %[1]s
+`
+
 	completionFlags = []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "kubectl",
@@ -21,18 +64,19 @@ var (
 		},
 		&cli.BoolFlag{
 			Name:  "ctr",
-			Usage: "(ctr) export containerd sock file",
+			Usage: "(ctr) export containerd sock file and set namespace",
 		},
 	}
 
-	k3sCompletionBase = mustCmdFromK3S(cmds.NewCompletionCommand(Run), K3SFlagSet{
-		"i": copyFlag,
-	})
+	k3sCompletionBase = mustCmdFromK3S(cmds.NewCompletionCommand(Bash, Zsh), K3SFlagSet{})
 )
 
 func NewCompletionCommand() *cli.Command {
 	cmd := k3sCompletionBase
-	cmd.Flags = append(cmd.Flags, completionFlags...)
+	for _, command := range cmd.Subcommands {
+		command.Flags = append(command.Flags, completionFlags...)
+	}
+
 	return cmd
 }
 
@@ -57,70 +101,27 @@ func isCtrSet(ctr bool) string {
 	return ""
 }
 
-func Run(ctx *cli.Context) error {
-	if ctx.NArg() < 1 {
-		return fmt.Errorf("must provide a valid SHELL argument")
-	}
-	shell := ctx.Args().First()
-	completetionScript, err := genCompletionScript(shell, ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
-	if err != nil {
-		return err
-	}
+func Bash(ctx *cli.Context) error {
+	completetionScript := genCompletionScript(bashScript, ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
 	if ctx.Bool("i") {
-		return writeToRC(shell, ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
+		return writeToRC("bash", "/.bashrc", ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
 	}
 	fmt.Println(completetionScript)
 	return nil
 }
 
-func genCompletionScript(shell string, kubectl, crictl, ctr bool) (string, error) {
-	var completionScript string
-	if shell == "bash" {
-		completionScript = fmt.Sprintf(`#! /bin/bash
-_cli_bash_autocomplete() {
-if [[ "${COMP_WORDS[0]}" != "source" ]]; then
-	local cur opts base
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	if [[ "$cur" == "-"* ]]; then
-	opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
-	else
-	opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
-	fi
-	COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-	return 0
-fi
-}
-
-complete -o bashdefault -o default -o nospace -F _cli_bash_autocomplete %s
-`, version.Program)
-	} else if shell == "zsh" {
-		completionScript = fmt.Sprintf(`#compdef %[1]s
-_cli_zsh_autocomplete() {
-
-	local -a opts
-	local cur
-	cur=${words[-1]}
-	if [[ "$cur" == "-"* ]]; then
-	opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
-	else
-	opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
-	fi
-
-	if [[ "${opts[1]}" != "" ]]; then
-	_describe 'values' opts
-	else
-	_files
-	fi
-
-	return
-}
-
-compdef _cli_zsh_autocomplete %[1]s
-`, version.Program)
-	} else {
-		return "", fmt.Errorf("unknown shell: %s", shell)
+func Zsh(ctx *cli.Context) error {
+	completetionScript := genCompletionScript(zshScript, ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
+	if ctx.Bool("i") {
+		return writeToRC("zsh", "/.zshrc", ctx.Bool("kubectl"), ctx.Bool("crictl"), ctx.Bool("ctr"))
 	}
+	fmt.Println(completetionScript)
+	return nil
+}
+
+func genCompletionScript(script string, kubectl, crictl, ctr bool) string {
+	var completionScript string
+	completionScript = fmt.Sprintf(script, version.Program)
 
 	if kubectl {
 		completionScript = fmt.Sprintf(`%s
@@ -137,19 +138,15 @@ export CRI_CONFIG_FILE=/var/lib/rancher/rke2/agent/etc/crictl.yaml
 	if ctr {
 		completionScript = fmt.Sprintf(`%s
 export CONTAINERD_ADDRESS=/run/k3s/containerd/containerd.sock
+export CONTAINERD_NAMESPACE=k8s.io
     `, completionScript)
 	}
 
-	return completionScript, nil
+	return completionScript
 }
 
-func writeToRC(shell string, kubectl, crictl, ctr bool) error {
-	rcFileName := ""
-	if shell == "bash" {
-		rcFileName = "/.bashrc"
-	} else if shell == "zsh" {
-		rcFileName = "/.zshrc"
-	}
+func writeToRC(shell, fileName string, kubectl, crictl, ctr bool) error {
+	rcFileName := fileName
 
 	home, err := os.UserHomeDir()
 	if err != nil {
