57dcececc21667d623555e010d23f7f853435ecb
Brad Davidson <brad.davidson@rancher.com>
2025-09-10 21:34:16 +0000

    Fix runtimeclass ownership takeover
    Replace relabeling of runtimeclasses to transfer ownership to the chart, with native helm --take-ownership support, now that helm-controller supports it
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- charts/build-charts.sh
+++ charts/build-charts.sh
@@ -3,6 +3,6 @@
 set -eux -o pipefail
 CHARTS_DIR=$(dirname $0)
 
-while read version filename bootstrap; do
-  CHART_VERSION=$version CHART_FILE=$CHARTS_DIR/$(basename $filename) CHART_BOOTSTRAP=$bootstrap $CHARTS_DIR/build-chart.sh
-done <<< $(yq e '.charts[] | [.version, .filename, .bootstrap] | join(" ")' $CHARTS_DIR/chart_versions.yaml)
+while read version filename bootstrap take_ownership; do
+  CHART_VERSION=$version CHART_FILE=$CHARTS_DIR/$(basename $filename) CHART_BOOTSTRAP=$bootstrap CHART_TAKE_OWNERSHIP=$take_ownership $CHARTS_DIR/build-chart.sh
+done <<< $(yq e '.charts[] | [.version, .filename, .bootstrap, .takeOwnership] | join(" ")' $CHARTS_DIR/chart_versions.yaml)
