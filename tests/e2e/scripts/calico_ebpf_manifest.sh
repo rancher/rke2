4aa991f58f3e1cc5ccf9dbae890b2fae497ef14c
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-12-15 14:49:35 +0000

    Add e2e test for Calico in eBPF mode

---
+++ tests/e2e/scripts/calico_ebpf_manifest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ip4_addr=$1
+
+# Set Calico parameters to use the eBPF dataplane instead of iptables
+mkdir -p /var/lib/rancher/rke2/server/manifests
+
+echo "Creating calico chart"
+echo "apiVersion: helm.cattle.io/v1
+kind: HelmChartConfig
+metadata:
+  name: rke2-calico
+  namespace: kube-system
+spec:
+  valuesContent: |-
+    installation:
+      calicoNetwork:
+        nodeAddressAutodetectionV4:
+          canReach: $ip4_addr 
+        bpfNetworkBootstrap: Enabled
+        kubeProxyManagement: Enabled
+        linuxDataplane: BPF
+    kubernetesServiceEndpoint:
+      host: localhost" >> /var/lib/rancher/rke2/server/manifests/e2e-calico.yaml
