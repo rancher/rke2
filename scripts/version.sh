758a3e2229af36cfcd707d81ddeba8be65fb14c6
Derek Nola <derek.nola@suse.com>
2025-12-15 14:56:30 -0800

    Override DefaultHelmJob at build time
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,6 +5,7 @@ PROG=rke2
 REGISTRY=${REGISTRY:-docker.io}
 REPO=${REPO:-rancher}
 K3S_PKG=github.com/k3s-io/k3s
+HELMCTR_PKG=github.com/k3s-io/helm-controller
 RKE2_PKG=github.com/rancher/rke2
 GO=${GO-go}
 GOARCH=${GOARCH:-$("${GO}" env GOARCH)}
@@ -37,6 +38,7 @@ KUBERNETES_VERSION=${KUBERNETES_VERSION:-v1.34.3}
 KUBERNETES_IMAGE_TAG=${KUBERNETES_IMAGE_TAG:-v1.34.3-rke2r1-build20251210}
 PAUSE_VERSION=${PAUSE_VERSION:-3.6}
 CCM_VERSION=${CCM_VERSION:-v1.33.0-rc1.0.20251210002518-764e98cbb4c1-build20251210}
+KLIPPERHELM_VERSION=${KLIPPERHELM_VERSION:-v0.9.12-build20251215}
 
 if [ -d .git ]; then
     if [ -z "$GIT_TAG" ]; then
