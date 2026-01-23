2126b900bc812ac44d9092063fe84fc478d67a02
Manuel Buil <mbuil@suse.com>
2026-01-23 16:34:44 +0100

    Consolidate vagrant network configuration in one script
    Signed-off-by: Manuel Buil <mbuil@suse.com>
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- tests/e2e/scripts/cni-setup.sh
+++ tests/e2e/scripts/cni-setup.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
-ip4_addr=$1
-ip6_addr=$2
-ip6_addr_gw=$3
-cni=$4
-os=$5
-
-sysctl -w net.ipv6.conf.all.disable_ipv6=0
-sysctl -w net.ipv6.conf.eth1.accept_dad=0
-
-
-
-if [ -z "${os##*ubuntu*}" ]; then
-  netplan set ethernets.eth1.accept-ra=false
-  netplan set ethernets.eth1.addresses=["$ip4_addr"/24,"$ip6_addr"/64]
-  netplan set ethernets.eth1.gateway6="$ip6_addr_gw"
-  netplan apply
-elif [ -z "${os##*alpine*}" ]; then
-  iplink set eth1 down
-  iplink set eth1 up
-  ip -6 addr add "$ip6_addr"/64 dev eth1
-  ip -6 r add default via "$ip6_addr_gw"
-else
-  ip -6 addr add "$ip6_addr"/64 dev eth1
-  ip -6 r add default via "$ip6_addr_gw"
-fi
-ip addr show dev eth1
-ip -6 r
-
-echo "net.ipv6.conf.all.disable_ipv6=0
-net.ipv6.conf.eth1.accept_dad=0" > /etc/sysctl.conf
+cni=$1
 
 # Override default CNI and specify the interface since we don't have a default IPv6 route
 mkdir -p /var/lib/rancher/rke2/server/manifests
