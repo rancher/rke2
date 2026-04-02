c9d37a0cb16fffee352f90f2e836f8df5cb99ffe
thomasferrandiz <thomas.ferrandiz@suse.com>
2026-04-02 18:23:50 +0200

    add checksum verification for 3rd party dependencies (#10081)

--- tests/e2e/scripts/install_sonobuoy.sh
+++ tests/e2e/scripts/install_sonobuoy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
+
+SONOBUOY_VERSION="0.56.0"
+SONOBUOY_CHECKSUM="01e35fbe2c402a31a766b3f46fe3280136f67d39e153c0b517d035b61608a28d"
+
+SONOBUOY_PLUGINS_COMMIT="9359729a5a5948250aae78cb8b4b0eda87c0caaf"
+
 git clone https://github.com/phillipsj/my-sonobuoy-plugins.git
-wget -q https://github.com/vmware-tanzu/sonobuoy/releases/download/v0.56.0/sonobuoy_0.56.0_linux_amd64.tar.gz
-tar -xvf sonobuoy_0.56.0_linux_amd64.tar.gz
+cd my-sonobuoy-plugins && git checkout "${SONOBUOY_PLUGINS_COMMIT}" && cd ..
+
+curl -fsSL "https://github.com/vmware-tanzu/sonobuoy/releases/download/v${SONOBUOY_VERSION}/sonobuoy_${SONOBUOY_VERSION}_linux_amd64.tar.gz" -o sonobuoy.tar.gz
+echo "${SONOBUOY_CHECKSUM}  sonobuoy.tar.gz" | sha256sum -c
+tar -xvf sonobuoy.tar.gz sonobuoy
 chmod +x sonobuoy && mv sonobuoy /usr/local/bin/sonobuoy
+rm -f sonobuoy.tar.gz
