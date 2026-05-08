52f07aed422956fad45616d9f817b1781277d639
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2026-05-08 21:08:43 +0200

    Fix rke2-calico-crd chart build
    Signed-off-by: Roberto Bonafiglia <roberto.bonafiglia@suse.com>

--- charts/build-charts.sh
+++ charts/build-charts.sh
@@ -3,6 +3,6 @@
 set -eux -o pipefail
 CHARTS_DIR=$(dirname $0)
 
-while read version filename bootstrap; do
-  CHART_VERSION=$version CHART_FILE=$CHARTS_DIR/$(basename $filename) CHART_BOOTSTRAP=$bootstrap $CHARTS_DIR/build-chart.sh
-done <<< $(yq e '.charts[] | [.version, .filename, .bootstrap] | join(" ")' $CHARTS_DIR/chart_versions.yaml)
+while IFS='|' read version filename bootstrap package; do
+  CHART_VERSION=$version CHART_FILE=$CHARTS_DIR/$(basename $filename) CHART_BOOTSTRAP=$bootstrap CHART_PACKAGE=$package $CHARTS_DIR/build-chart.sh
+done <<< $(yq e '.charts[] | [(.version // ""), (.filename // ""), (.bootstrap // ""), (.package // "")] | join("|")' $CHARTS_DIR/chart_versions.yaml)
