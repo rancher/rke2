72b11f0a24f3f822d0df88bd2d825c6394cc52ce
Derek Nola <derek.nola@suse.com>
2026-07-10 13:02:15 -0700

    Update updatecli/scripts/retrieve_chart_version.sh
    Co-authored-by: Brad Davidson <brad.davidson@rancher.com>

--- updatecli/scripts/retrieve_chart_version.sh
+++ updatecli/scripts/retrieve_chart_version.sh
@@ -11,7 +11,7 @@ CHART_INDEX_FILE_URL="https://rke2-charts.rancher.io/index.yaml"
 CHART_NAME="${1}"
 # Versions are unordered inside the charts file, so we must sort by version in
 # reverse order and get the highest.
-CHART_VERSION=$(curl -sfL "${CHART_INDEX_FILE_URL}" | yq -r '.entries["'"${CHART_NAME}"'"][].version' | sort -rV | head -n 1)
+CHART_VERSION=$(curl -sfL "${CHART_INDEX_FILE_URL}" | yq -r '.entries[env(CHART_NAME)][].version' | sort -rV | head -n 1)
 
 if [[ "${CHART_VERSION}" = "null" ]] || [[ -z "${CHART_VERSION}" ]]; then
     fatal "failed to retrieve the charts' index file or to parse it"
